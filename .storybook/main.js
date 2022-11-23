242d29b88de1d674afac4f5121ae5e6f6530ba55
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-11-23 10:17:43 +0100

    docs(storybook): enable essentials addon (#6)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,21 @@
 /* eslint-env node */
 
 module.exports = {
-  // addons: [
-  //   '@storybook/addon-actions',
-  //   '@storybook/addon-essentials',
-  //   '@storybook/addon-links',
-  //   '@storybook/addon-postcss'
-  // ],
-  // staticDirs: ['../public'],
+  addons: ['@storybook/addon-essentials'],
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   core: {
     builder: 'webpack5'
-  }
+  },
+  // Messy issue with assert resolving (related to docs generation), fixed in Storybook v7.
+  // https://github.com/storybookjs/storybook/issues/17458#issuecomment-1268464401
+  webpackFinal: config => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        assert: require.resolve('browser-assert')
+      }
+    }
+  })
 }
