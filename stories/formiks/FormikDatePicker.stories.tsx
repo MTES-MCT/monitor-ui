e255c70a4e2c6efcbca9d28ea545cf7863586033
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-11-24 13:45:34 +0100

    feat(formiks): add FormikDatePicker

--- stories/formiks/FormikDatePicker.stories.tsx
+++ stories/formiks/FormikDatePicker.stories.tsx
@@ -1,32 +1,31 @@
 import { Formik } from 'formik'
 import { useState } from 'react'
 
-import { FormikEffect, FormikDateRangePicker } from '../../src'
+import { FormikEffect, FormikDatePicker } from '../../src'
 import { Output } from '../_components/Output'
 import { noop } from '../_utils/noop'
 
-import type { FormikDateRangePickerProps } from '../../src'
-import type { DateRange } from '../../src/types'
+import type { FormikDatePickerProps } from '../../src'
 
 export default {
-  title: 'Formiks/FormikDateRangePicker',
-  component: FormikDateRangePicker,
+  title: 'Formiks/FormikDatePicker',
+  component: FormikDatePicker,
 
   argTypes: {},
 
   args: {
     isHistorical: false,
     isLabelHidden: false,
-    label: 'FormikDateRangePicker Label',
-    name: 'myDateRange',
+    label: 'FormikDatePicker Label',
+    name: 'myDate',
     withTime: false
   }
 }
 
-export const _FormikDateRangePicker = (props: FormikDateRangePickerProps) => {
+export const _FormikDatePicker = (props: FormikDatePickerProps) => {
   const [outputValue, setOutputValue] = useState<
     | {
-        myDateRange?: DateRange
+        myDate?: Date
       }
     | '∅'
   >('∅')
@@ -37,7 +36,7 @@ export const _FormikDateRangePicker = (props: FormikDateRangePickerProps) => {
         <>
           <FormikEffect onChange={setOutputValue} />
 
-          <FormikDateRangePicker {...props} />
+          <FormikDatePicker {...props} />
         </>
       </Formik>
 
