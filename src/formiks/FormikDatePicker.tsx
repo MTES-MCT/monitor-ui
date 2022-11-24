a4db006c1f72a39fd7ea8dc2722399783c44826b
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-11-24 13:49:24 +0100

    feat(formiks): add FormikDatePicker (#16)

---
+++ src/formiks/FormikDatePicker.tsx
@@ -0,0 +1,21 @@
+import { useField } from 'formik'
+import { useEffect } from 'react'
+
+import { DatePicker } from '../fields/DatePicker'
+
+import type { DatePickerProps } from '../fields/DatePicker'
+
+export type FormikDatePickerProps = Omit<DatePickerProps, 'onChange'> & {
+  name: string
+}
+export function FormikDatePicker({ name, ...originalProps }: FormikDatePickerProps) {
+  const [, , helpers] = useField(name)
+  const { setValue } = helpers
+
+  // We don't include `setValues` in  `useEffect()` dependencies
+  // both because it is useless and it will trigger infinite hook calls
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => () => setValue(undefined), [])
+
+  return <DatePicker onChange={setValue} {...originalProps} />
+}
