1d5ec1b06b0f9ed06b44518de60cd69f0286e8fb
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-12-01 16:25:56 +0100

    feat(components): add Dropdown (#93)

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,82 @@
+import { useMemo } from 'react'
+import { Dropdown as RsuiteDropdown } from 'rsuite'
+import styled from 'styled-components'
+
+import { Item } from './Item'
+
+import type { IconProps } from '../../types'
+import type { DropdownItemProps } from './Item'
+import type { FunctionComponent } from 'react'
+import type { DropdownProps as RsuiteDropdownProps } from 'rsuite'
+
+export type DropdownProps = Omit<RsuiteDropdownProps, 'as' | 'icon'> & {
+  Icon?: FunctionComponent<IconProps>
+}
+function RawDropdown({ Icon, ...originalProps }: DropdownProps) {
+  const icon = useMemo(() => (Icon ? <Icon size={1.25} /> : undefined), [Icon])
+  const hasIcon = useMemo(() => Boolean(Icon), [Icon])
+
+  return <StyledDropdown hasIcon={hasIcon} icon={icon} {...originalProps} />
+}
+
+// TODO We need to split into multiple styled components as done in `<Button />`.
+const StyledDropdown = styled(RsuiteDropdown)<{
+  hasIcon: boolean
+}>`
+  .rs-btn {
+    align-items: center;
+    background-color: ${p => p.theme.color.charcoal};
+    border: solid 1px ${p => p.theme.color.charcoal};
+    color: ${p => p.theme.color.gainsboro};
+    display: flex;
+    font-size: 13px;
+    padding: ${p => (p.hasIcon ? '5px' : '6px')} 12px;
+
+    :hover {
+      background-color: ${p => p.theme.color.blueYonder['100']};
+      border: 1px solid ${p => p.theme.color.blueYonder['100']};
+      color: ${p => p.theme.color.white};
+    }
+
+    :active {
+      background-color: ${p => p.theme.color.blueGray['100']};
+      border: 1px solid ${p => p.theme.color.blueGray['100']};
+      color: ${p => p.theme.color.white};
+    }
+
+    :disabled {
+      background-color: ${p => p.theme.color.lightGray};
+      border: 1px solid ${p => p.theme.color.lightGray};
+      color: ${p => p.theme.color.cultured};
+    }
+
+    /* SVG Icon Components are wrapped within a <div /> */
+    > div {
+      margin-right: 0.5rem;
+    }
+
+    > svg {
+      display: none;
+    }
+  }
+
+  > .rs-dropdown-menu {
+    border-radius: 0;
+    padding: 0;
+  }
+
+  svg.rs-dropdown-item-menu-icon {
+    vertical-align: middle;
+    margin-right: 10px;
+  }
+`
+
+RawDropdown.displayName = 'Dropdown'
+
+export const Dropdown: FunctionComponent<DropdownProps> & {
+  Item: FunctionComponent<DropdownItemProps>
+} = Object.assign(RawDropdown, {
+  Item
+})
+
+export { DropdownItemProps }
---
+++ stories/components/Dropdown.stories.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import { Dropdown, Icon } from '../../src'
+import { Output } from '../_components/Output'
+
+import type { DropdownProps } from '../../src'
+
+const args: DropdownProps = {
+  title: 'A dropdow menu'
+}
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+
+  argTypes: {},
+
+  args
+}
+
+export const _Dropdown = (props: DropdownProps) => {
+  const [outputValue, setOutputValue] = useState<boolean | '∅'>('∅')
+
+  return (
+    <Box>
+      <Dropdown open {...props} onSelect={setOutputValue}>
+        <Dropdown.Item eventKey="FIRST_MENU_ITEM">First menu item</Dropdown.Item>
+        <Dropdown.Item eventKey="SECOND_MENU_ITEM">Second menu item</Dropdown.Item>
+        <Dropdown.Item eventKey="THIRD_MENU_ITEM">Third menu item</Dropdown.Item>
+        <Dropdown.Item eventKey="A_VERY_VERY_LONG_MENU_ITEM">A very very long menu item</Dropdown.Item>
+      </Dropdown>
+
+      <Dropdown Icon={Icon.Plus} onSelect={setOutputValue} open title="A dropdown menu with icons">
+        <Dropdown.Item eventKey="WE_FOUND_NEMO" Icon={Icon.Fishery}>
+          We found Nemo!
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="SECOND_MENU_ITEM" Icon={Icon.FleetSegment}>
+          A fancy boat
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="THIRD_MENU_ITEM" Icon={Icon.Search}>
+          Search a soul
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="A_VERY_VERY_LONG_MENU_ITEM" Icon={Icon.Delete}>
+          Delete the entire universe
+        </Dropdown.Item>
+      </Dropdown>
+
+      {outputValue !== '∅' && <Output value={outputValue} />}
+    </Box>
+  )
+}
+
+const Box = styled.div`
+  > div:nth-child(2) {
+    margin-left: 5rem;
+  }
+`
