8a3d6537484f8a0b85289f6903e5567cdef0673b
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-12-15 02:01:06 +0100

    feat(fields): add MultiZoneEditor (#135)

---
+++ src/fields/MultiZoneEditor/index.tsx
@@ -0,0 +1,151 @@
+// TODO Clean, split and finalize this component.
+
+import { equals, remove } from 'ramda'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+
+import { Accent } from '../../constants'
+import { Button } from '../../elements/Button'
+import { Fieldset } from '../../elements/Fieldset'
+import { IconButton } from '../../elements/IconButton'
+import { Delete, Edit, Plus, SelectRectangle } from '../../icons'
+
+import type { Promisable } from 'type-fest'
+
+export type MultiZoneEditorProps = {
+  addButtonLabel: string
+  defaultValue?: Record<string, any>[]
+  initialZone: Record<string, any>
+  isLabelHidden?: boolean
+  isLight?: boolean
+  label: string
+  labelPropName: string
+  onAdd?: (nextZones: Record<string, any>[], index: number) => Promisable<void>
+  onCenter?: (zone: Record<string, any>) => Promisable<void>
+  onDelete?: (nextZones: Record<string, any>[]) => Promisable<void>
+  onEdit?: (zone: Record<string, any>, index: number) => Promisable<void>
+}
+export function MultiZoneEditor({
+  addButtonLabel,
+  defaultValue = [],
+  initialZone,
+  isLabelHidden = false,
+  isLight = false,
+  label,
+  labelPropName,
+  onAdd,
+  onCenter,
+  onDelete,
+  onEdit
+}: MultiZoneEditorProps) {
+  const prevDefaultValueRef = useRef(defaultValue)
+
+  const [zones, setZones] = useState(defaultValue)
+
+  const addZone = useCallback(() => {
+    const nextZones = [...zones, initialZone]
+
+    if (onAdd) {
+      onAdd(nextZones, nextZones.length)
+    }
+
+    setZones(nextZones)
+  }, [initialZone, onAdd, zones])
+
+  const centerZone = useCallback(
+    (zone: Record<string, any>) => {
+      if (onCenter) {
+        onCenter(zone)
+      }
+    },
+    [onCenter]
+  )
+
+  const deleteZone = useCallback(
+    (index: number) => {
+      const nextZones = remove(index, 1, zones)
+
+      setZones(nextZones)
+
+      if (onDelete) {
+        onDelete(nextZones)
+      }
+    },
+    [onDelete, zones]
+  )
+
+  const editZone = useCallback(
+    (index: number, zone: Record<string, any>) => {
+      if (onEdit) {
+        onEdit(zone, index)
+      }
+    },
+    [onEdit]
+  )
+
+  useEffect(() => {
+    if (equals(defaultValue, prevDefaultValueRef.current)) {
+      return
+    }
+
+    setZones(defaultValue)
+  }, [defaultValue])
+
+  return (
+    <Fieldset isHidden={isLabelHidden} isLight={isLight} legend={label}>
+      <Button accent={Accent.SECONDARY} Icon={Plus} isFullWidth onClick={addZone}>
+        {addButtonLabel}
+      </Button>
+
+      <>
+        {zones.map((zone, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Row key={`zone-${index}`}>
+            <ZoneWrapper>
+              {zone[labelPropName]}
+
+              {/* TODO Add `Accent.LINK` accent in @mtes-mct/monitor-ui and use it here. */}
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid, jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+              <a
+                onClick={() => centerZone(zone)}
+                style={{
+                  cursor: 'pointer'
+                }}
+              >
+                <SelectRectangle /> Centrer sur la carte
+              </a>
+            </ZoneWrapper>
+
+            <IconButton accent={Accent.SECONDARY} Icon={Edit} onClick={() => editZone(index, zone)} />
+            <IconButton
+              accent={Accent.SECONDARY}
+              aria-label="Supprimer cette zone"
+              Icon={Delete}
+              onClick={() => deleteZone(index)}
+            />
+          </Row>
+        ))}
+      </>
+    </Fieldset>
+  )
+}
+
+const Row = styled.div`
+  align-items: center;
+  display: flex;
+  margin: 8px 0 0;
+
+  > button {
+    margin: 0 0 0 8px;
+  }
+`
+
+const ZoneWrapper = styled.div`
+  background-color: ${p => p.theme.color.gainsboro};
+  display: flex;
+  flex-grow: 1;
+  font-size: 13px;
+  line-height: 1.5;
+  justify-content: space-between;
+  padding: 6px 12px 6px;
+`
