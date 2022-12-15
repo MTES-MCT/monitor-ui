a75c0e560854d29f9a794144402d7aec433b9fd2
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-12-15 01:57:19 +0100

    feat(fields): add MultiZoneEditor

---
+++ stories/fields/MultiZoneEditor.stories.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import { Output } from '../../.storybook/components/Output'
+import { generateStoryDecorator } from '../../.storybook/components/StoryDecorator'
+import { MultiZoneEditor } from '../../src'
+
+import type { MultiZoneEditorProps } from '../../src'
+
+const args: MultiZoneEditorProps = {
+  addButtonLabel: 'Add a zone',
+  defaultValue: undefined,
+  initialZone: {
+    name: 'Polygone dessiné'
+  },
+  isLabelHidden: false,
+  isLight: false,
+  label: 'Some zones',
+  labelPropName: 'name'
+}
+
+export default {
+  title: 'Fields/MultiZoneEditor',
+  component: MultiZoneEditor,
+
+  argTypes: {
+    defaultValue: {
+      control: 'inline-radio',
+      options: ['FIRST_OPTION', 'SECOND_OPTION', 'THIRD_OPTION', 'A_VERY_VERY_LONG_OPTION']
+    }
+  },
+
+  args,
+
+  decorators: [
+    generateStoryDecorator({
+      hasDarkMode: true
+    })
+  ]
+}
+
+export function _MultiZoneEditor(props: MultiZoneEditorProps) {
+  const [outputValue, setOutputValue] = useState<Record<string, any>[] | undefined | '∅'>('∅')
+
+  return (
+    <>
+      <MultiZoneEditor {...props} onAdd={setOutputValue} onDelete={setOutputValue} />
+
+      {outputValue !== '∅' && <Output value={outputValue} />}
+    </>
+  )
+}
