1c9669ec96f639bb55e364f2c3b6200d65eab83e
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-02-09 00:57:06 +0100

    fix(fields): force error style props as important

--- src/elements/FieldError.tsx
+++ src/elements/FieldError.tsx
@@ -5,13 +5,14 @@ import type { HTMLAttributes } from 'react'
 export type FieldErrorProps = HTMLAttributes<HTMLParagraphElement> & {
   isDisabled?: boolean | undefined
 }
+// TODO This is bad to use `!important` in CSS. Let's rework this quick fix later on.
 export const FieldError = styled.p<{
   isDisabled?: boolean | undefined
 }>`
-  color: ${p => p.theme.color.maximumRed};
-  display: ${p => (p.isDisabled ? 'none' : 'block')};
-  font-size: 13px;
-  font-weight: 700;
-  line-height: 1.3846;
-  margin-top: 4px;
+  color: ${p => p.theme.color.maximumRed} !important;
+  display: ${p => (p.isDisabled ? 'none' : 'block')} !important;
+  font-size: 13px !important;
+  font-weight: 700 !important;
+  line-height: 1.3846 !important;
+  margin-top: 4px !important;
 `
