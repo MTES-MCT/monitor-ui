5ded9f1d97eaf684101ec74324fdd674b627b11e
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-02-09 02:38:18 +0100

    fix(formiks): initialize FormikCheckbox form value on mount

--- src/formiks/FormikCheckbox.tsx
+++ src/formiks/FormikCheckbox.tsx
@@ -1,5 +1,5 @@
 import { useField } from 'formik'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 
 import { Checkbox } from '../fields/Checkbox'
 
@@ -13,6 +13,17 @@ export function FormikCheckbox({ name, ...originalProps }: FormikCheckboxProps) 
   const handleChange = useMemo(() => helpers.setValue, [])
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const isDefaultChecked = useMemo(() => Boolean(field.value), [])
+
+  // A checkbox must initialize its Formik value on mount:
+  // it wouldn't make sense to keep it as `undefined` since `undefined` means `false` in the case of a checkbox
+  useEffect(
+    () => {
+      helpers.setValue(isDefaultChecked)
+    },
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
 
   return (
     <Checkbox
