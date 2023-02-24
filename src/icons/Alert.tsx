24135f96446f2482a7b86ba7398be7f25cf29931
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-02-24 10:37:51 +0100

    feat(icons): update

--- src/icons/Alert.tsx
+++ src/icons/Alert.tsx
@@ -6,14 +6,11 @@ export function Alert({ color, size, ...nativeProps }: IconProps) {
   return (
     <IconBox $color={color} $size={size}>
       <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
-        <g transform="translate(-220 -208)">
-          <path
-            d="M295,13V8a5,5,0,0,0-4-4.9V1h-2V3.1A5,5,0,0,0,285,8v5l-2,2v2h4.5a2.5,2.5,0,0,0,5,0H297V15Zm-5,5a1,1,0,0,1-1-1h2A1,1,0,0,1,290,18Zm1.486-3H285l2-2V8a3,3,0,0,1,6,0v5l2,2Z"
-            fill="currentColor"
-            transform="translate(-60 208)"
-          />
-          <rect fill="none" height="20" transform="translate(220 208)" width="20" />
-        </g>
+        <rect fill="none" height="20" width="20" />
+        <path
+          d="M18,14.037l-2.059-1.98V7.82A5.528,5.528,0,0,0,11.5,2.58V0h-3V2.568A5.539,5.539,0,0,0,4.059,7.82v4.237L2,14.037v2.349H6.755c-.007.091-.019.181-.018.273a3.264,3.264,0,1,0,6.526,0c0-.092-.012-.182-.019-.273H18Zm-6.737,2.622a1.263,1.263,0,1,1-2.526,0,1.327,1.327,0,0,1,.027-.269h2.471A1.366,1.366,0,0,1,11.263,16.659Zm.455-2.322-4.124-.006,0,.006H4.61l1.45-1.394V7.82c0-1.863,1.749-3.379,3.981-3.379a3.671,3.671,0,0,1,3.9,3.379v5.123l1.45,1.394Z"
+          fill="currentColor"
+        />
       </svg>
     </IconBox>
   )
