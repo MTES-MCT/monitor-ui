a1dfc985f87b32b2797a00f46ffef4dba4a16101
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-03-17 08:59:14 +0100

    feat(elements): add classname .Field to Field

--- src/elements/Field.tsx
+++ src/elements/Field.tsx
@@ -1,9 +1,12 @@
+import classNames from 'classnames'
 import styled from 'styled-components'
 
 import type { HTMLAttributes } from 'react'
 
 export type FieldProps = HTMLAttributes<HTMLDivElement>
-export const Field = styled.div`
+export const Field = styled.div.attrs<FieldProps>(props => ({
+  className: classNames('Field', props.className)
+}))`
   align-items: flex-start;
   display: flex;
   flex-direction: column;
