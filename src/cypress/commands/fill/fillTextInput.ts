1d47284e8aece44192e83c81ba71f685eb33f9d3
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-03-30 13:02:17 +0200

    fix(cypress): force text input typing in fill command

--- src/cypress/commands/fill/fillTextInput.ts
+++ src/cypress/commands/fill/fillTextInput.ts
@@ -1,6 +1,6 @@
 export function fillTextInput(textInputElement: HTMLInputElement, value: string | undefined) {
   if (value) {
-    cy.wrap(textInputElement).scrollIntoView().type(value)
+    cy.wrap(textInputElement).scrollIntoView().type(value, { force: true })
   } else {
     cy.wrap(textInputElement).scrollIntoView().clear()
   }
