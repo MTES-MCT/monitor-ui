4c5ed56d50ed293f173df31bd191ae32ff55220f
Claire Dagan <clairedagan@gmail.com>
2023-04-27 15:46:45 +0200

    feat(elements): add SimpleTable

---
+++ src/elements/Table/SimpleTable.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const StyledTable = styled.table`
+  width: 100%;
+  table-layout: auto;
+  overflow: auto;
+`
+export const StyledHead = styled.thead`
+  position: sticky;
+  top: 0;
+`
+
+export const StyledSortContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+export const StyledTh = styled.th`
+  background-color: ${p => p.theme.color.gainsboro};
+  border: 1px solid ${p => p.theme.color.lightGray};
+  color: ${p => p.theme.color.slateGray};
+  font-size: 13px;
+  font-weight: 500;
+  padding: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+export const StyledBodyTr = styled.tr`
+  :hover {
+    background-color: ${p => p.theme.color.blueYonder[25]};
+  }
+  td:first-child,
+  td:nth-child(2) {
+    width: 180px;
+  }
+
+  td:nth-child(3) {
+    width: 90px;
+  }
+  td:nth-child(4),
+  td:nth-child(7) {
+    max-width: 200px;
+  }
+
+  td:nth-child(5),
+  td:nth-child(6),
+  td:nth-child(8) {
+    width: 100px;
+  }
+
+  td:nth-child(9) {
+    width: 120px;
+  }
+  td:nth-child(10) {
+    width: 60px;
+  }
+  td:nth-child(11) {
+    width: 160px;
+    text-align: center;
+  }
+`
+
+export const StyledTd = styled.td`
+  font-size: 13px;
+  font-weight: 500;
+  color: ${p => p.theme.color.gunMetal};
+  text-align: left;
+  border: 1px solid ${p => p.theme.color.lightGray};
+  overflow: hidden;
+  padding: 10px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
---
+++ stories/table/SimpleTable.stories.tsx
@@ -0,0 +1,298 @@
+import { flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from '@tanstack/react-table'
+import { useVirtualizer } from '@tanstack/react-virtual'
+import { useCallback, useMemo, useRef, useState } from 'react'
+
+import { generateStoryDecorator } from '../../.storybook/components/StoryDecorator'
+import { Accent, Button, Icon, IconButton, Size } from '../../src'
+import {
+  StyledBodyTr,
+  StyledHead,
+  StyledSortContainer,
+  StyledTable,
+  StyledTd,
+  StyledTh
+} from '../../src/elements/Table/SimpleTable'
+
+const fakeData1 = Array(100).fill({
+  closedBy: 'TIM',
+  controlUnits: [
+    {
+      administration: 'DDTM',
+      contact: undefined,
+      id: 10003,
+      isArchived: false,
+      name: 'DML 2A',
+      resources: []
+    }
+  ],
+  endDateTimeUtc: '19 juin 23, 09h28 (UTC)',
+  envActions: [
+    {
+      actionNumberOfControls: 1,
+      actionStartDateTimeUtc: undefined,
+      actionTargetType: 'COMPANY',
+      actionType: 'CONTROL',
+      geom: { type: 'MultiPoint', coordinates: [[-2.63708273, 48.17060427]] },
+      id: 'f3e90d3a-6ba4-4bb3-805e-d391508aa46d',
+      infractions: [],
+      observations: undefined,
+      themes: [
+        {
+          protectedSpecies: ['MARINE_MAMMALS'],
+          subThemes: ['Dérogations concernant les espèces protégées'],
+          theme: 'Police des espèces protégées et de leurs habitats (faune et flore)'
+        }
+      ],
+      vehicleType: undefined
+    }
+  ],
+  facade: 'Guyane',
+  geom: { type: 'MultiPolygon' },
+  hasMissionOrder: false,
+  id: 38,
+  isClosed: true,
+  isUnderJdp: false,
+  missionNature: ['ENV', 'FISH'],
+  missionSource: 'MONITORENV',
+  missionTypes: ['LAND'],
+  observationsCacem: 'Black bit sell. House relate policy once. White member worker east even anyone detail professor.',
+  observationsCnsp: undefined,
+  openBy: 'RAN',
+  startDateTimeUtc: '23 juin 23, 05h57 (UTC)'
+})
+const fakeData2 = Array(100).fill({
+  closedBy: 'TIM',
+  controlUnits: [
+    {
+      administration: 'DDTM',
+      contact: undefined,
+      id: 10003,
+      isArchived: false,
+      name: 'DML 2A',
+      resources: []
+    }
+  ],
+  endDateTimeUtc: '23 juin 23, 09h28 (UTC)',
+  envActions: [
+    {
+      actionNumberOfControls: 1,
+      actionStartDateTimeUtc: undefined,
+      actionTargetType: 'COMPANY',
+      actionType: 'CONTROL',
+      geom: { type: 'MultiPoint', coordinates: [[-2.63708273, 48.17060427]] },
+      id: 'f3e90d3a-6ba4-4bb3-805e-d391508aa46d',
+      infractions: [],
+      observations: undefined,
+      themes: [
+        {
+          protectedSpecies: ['MARINE_MAMMALS'],
+          subThemes: ['Dérogations concernant les espèces protégées'],
+          theme: 'Police des espèces protégées et de leurs habitats (faune et flore)'
+        }
+      ],
+      vehicleType: undefined
+    }
+  ],
+  facade: 'Guadeloupe',
+  geom: { type: 'MultiPolygon' },
+  hasMissionOrder: false,
+  id: 38,
+  isClosed: true,
+  isUnderJdp: false,
+  missionNature: ['ENV', 'FISH'],
+  missionSource: 'MONITORENV',
+  missionTypes: ['LAND'],
+  observationsCacem: 'Black bit sell. House relate policy once. White member worker east even anyone detail professor.',
+  observationsCnsp: undefined,
+  openBy: 'RAN',
+  startDateTimeUtc: '01 mai 23, 05h57 (UTC)'
+})
+
+const data = [...fakeData1, ...fakeData2]
+
+export default {
+  title: 'Table/SimpleTable',
+  decorators: [generateStoryDecorator()]
+}
+
+export function _SimpleTable() {
+  const [sorting, setSorting] = useState<SortingState>([{ id: 'dateDebut', desc: false }])
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorFn: row => row.startDateTimeUtc,
+        id: 'dateDebut',
+        cell: info => info.getValue(),
+        header: () => 'Début'
+      },
+      {
+        accessorFn: row => row.endDateTimeUtc,
+        id: 'dateFin',
+        cell: info => info.getValue(),
+        header: () => 'Fin'
+      },
+      {
+        accessorFn: row => row.missionSource,
+        id: 'missionSource',
+        cell: info => info.getValue(),
+        header: () => 'Origine'
+      },
+      {
+        accessorFn: row =>
+          row?.controlUnits?.map(controlUnit => `${controlUnit.name} (${controlUnit.administration})`).join(' / '),
+        id: 'Unit and Administration',
+        cell: info => info.getValue(),
+        header: () => 'Unité (Administration)',
+        enableSorting: false
+      },
+      {
+        accessorFn: row => row.missionTypes,
+        id: 'type',
+        cell: info => info.getValue(),
+        header: () => 'Type',
+        enableSorting: false
+      },
+      {
+        accessorFn: row => row.facade,
+        id: 'seaFront',
+        cell: info => info.getValue(),
+        header: () => 'Facade'
+      },
+      {
+        accessorFn: row => row.observationsCacem,
+        id: 'observationsCacem',
+        cell: info => info.getValue(),
+        header: () => 'Thématiques',
+        enableSorting: false
+      },
+      {
+        accessorFn: row => row.controlUnits.length,
+        id: 'controls',
+        cell: info => info.getValue(),
+        header: () => 'Nbre contrôles',
+        enableSorting: false
+      },
+      {
+        accessorFn: row => row.status,
+        id: 'status',
+        cell: info => info.getValue(),
+        header: () => 'Statut',
+        enableSorting: false
+      },
+      {
+        accessorFn: row => row.geom,
+        id: 'geom',
+        cell: info => (
+          <IconButton
+            accent={Accent.SECONDARY}
+            Icon={Icon.FocusZones}
+            onClick={() => console.log(info.getValue())}
+            size={Size.SMALL}
+          />
+        ),
+        header: () => '',
+        enableSorting: false
+      },
+      {
+        accessorFn: row => row.id,
+        id: 'edit',
+        cell: info => (
+          <Button Icon={Icon.Edit} onClick={() => console.log(info.getValue())} size={Size.SMALL}>
+            Editer
+          </Button>
+        ),
+        header: () => '',
+        enableSorting: false
+      }
+    ],
+    []
+  )
+
+  const table = useReactTable({
+    columns,
+    data,
+    state: {
+      sorting
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel()
+  })
+
+  const tableContainerRef = useRef<HTMLDivElement>(null)
+
+  const { rows } = table.getRowModel()
+  const rowVirtualizer = useVirtualizer({
+    count: rows.length,
+    getScrollElement: () => tableContainerRef.current,
+    estimateSize: () => 40,
+    overscan: 10,
+    // Pass correct keys to virtualizer it's important when rows change position
+    getItemKey: useCallback((index: number) => `${rows[index]?.id}`, [rows])
+  })
+
+  const virtualRows = rowVirtualizer.getVirtualItems()
+  const [paddingTop, paddingBottom] =
+    virtualRows.length > 0
+      ? [
+          Math.max(0, virtualRows[0]?.start || 0),
+          Math.max(0, rowVirtualizer.getTotalSize() - (virtualRows[virtualRows.length - 1]?.end || 0))
+        ]
+      : [0, 0]
+
+  return (
+    <div ref={tableContainerRef}>
+      <StyledTable>
+        <StyledHead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <StyledTh key={header.id}>
+                  {header.isPlaceholder ? undefined : (
+                    <StyledSortContainer
+                      {...{
+                        className: header.column.getCanSort() ? 'cursor-pointer select-none' : '',
+                        onClick: header.column.getToggleSortingHandler()
+                      }}
+                    >
+                      {flexRender(header.column.columnDef.header, header.getContext())}
+                      {header.column.getCanSort() &&
+                        ({
+                          asc: <Icon.Close />,
+                          desc: <Icon.Chevron />
+                        }[header.column.getIsSorted() as string] ?? <Icon.SortingArrows size="14" />)}
+                    </StyledSortContainer>
+                  )}
+                </StyledTh>
+              ))}
+            </tr>
+          ))}
+        </StyledHead>
+        <tbody>
+          {paddingTop > 0 && (
+            <tr>
+              <td style={{ height: `${paddingTop}px` }} />
+            </tr>
+          )}
+          {virtualRows.map(virtualRow => {
+            const row = rows[virtualRow.index]
+
+            return (
+              <StyledBodyTr key={virtualRow.key}>
+                {row?.getVisibleCells().map(cell => (
+                  <StyledTd key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</StyledTd>
+                ))}
+              </StyledBodyTr>
+            )
+          })}
+          {paddingBottom > 0 && (
+            <tr>
+              <td style={{ height: `${paddingBottom}px` }} />
+            </tr>
+          )}
+        </tbody>
+      </StyledTable>
+    </div>
+  )
+}
