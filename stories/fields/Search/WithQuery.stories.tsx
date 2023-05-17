68c1570eea587da47120c929db4b2371d78d614a
Claire Dagan <clairedagan@gmail.com>
2023-05-17 16:33:01 +0200

    fix(fields): delete queryMap and queryUrl props

--- stories/fields/Search/WithQuery.stories.tsx
+++ stories/fields/Search/WithQuery.stories.tsx
@@ -1,3 +1,4 @@
+import ky from 'ky'
 import { useState } from 'react'
 
 import { Output } from '../../../.storybook/components/Output'
@@ -12,14 +13,7 @@ const args: SearchProps = {
   isLight: false,
   label: 'An autocompletable select',
   name: 'autoComplete',
-  placeholder: 'Type "brew"',
-  throttleDuration: 500,
-  options: [],
-  queryMap: ({ id, name }) => ({
-    label: name,
-    value: id
-  }),
-  queryUrl: 'https://api.openbrewerydb.org/breweries?by_name=%s'
+  placeholder: 'Type "brew"'
 }
 
 export default {
@@ -39,10 +33,25 @@ export default {
 
 export function WithQuery(props: SearchProps) {
   const [outputValue, setOutputValue] = useState<any | undefined | '∅'>('∅')
+  const [options, setOptions] = useState<{ label: any; value: any }[]>([])
+
+  const onQuery = async value => {
+    const results: Record<string, any>[] = await ky
+      .get(`https://api.openbrewerydb.org/breweries?by_name=${value}`)
+      .json()
+
+    const dataFormatted = results
+      ? results.map(({ id, name }) => ({
+          label: name,
+          value: id
+        }))
+      : []
+    setOptions(dataFormatted)
+  }
 
   return (
     <>
-      <Search {...props} onChange={setOutputValue} />
+      <Search {...props} onChange={setOutputValue} onQuery={onQuery} options={options} />
 
       {outputValue !== '∅' && <Output value={outputValue} />}
     </>
