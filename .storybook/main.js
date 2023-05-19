10826c4cd2f092af580a8efa068dd34f3214af2e
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-05-19 08:28:52 +0200

    build(dev-deps): migrate to Storybook v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,14 @@
 
 module.exports = {
   addons: ['@storybook/addon-essentials', 'storybook-addon-performance', '@storybook/addon-a11y'],
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  core: {
-    builder: 'webpack5'
+  docs: {
+    autodocs: true
   },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   // Messy issue with assert resolving (related to docs generation), fixed in Storybook v7.
   // https://github.com/storybookjs/storybook/issues/17458#issuecomment-1268464401
   webpackFinal: config => ({
