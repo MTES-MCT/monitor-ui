3a43861d57cdc261edd689c9c7f6ea832752bfa5
Claire Dagan <clairedagan@gmail.com>
2023-05-19 09:12:44 +0200

    feat(icons): update and add icons

--- src/icons/Landmark.tsx
+++ src/icons/Landmark.tsx
@@ -6,10 +6,20 @@ export function Landmark({ color, size, ...nativeProps }: IconProps) {
   return (
     <IconBox $color={color} $size={size}>
       <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
-        <g transform="translate(120 -82)">
-          <rect fill="currentColor" height="15" transform="translate(-114 99) rotate(180)" width="2" />
-          <path d="M-113,84V94l12-5Z" fill="currentColor" />
-          <rect fill="none" height="20" transform="translate(-120 82)" width="20" />
+        <defs>
+          <clipPath>
+            <rect fill="none" height="20" width="20" />
+          </clipPath>
+        </defs>
+        <g clipPath="url(#clip-path)">
+          <path
+            d="M10,1A6,6,0,0,0,4,7a6.108,6.108,0,0,0,.091,1.01c.007.038.011.078.018.116a5.968,5.968,0,0,0,.252.908c.016.045.032.091.05.135a6.061,6.061,0,0,0,.4.836l.06.1a5.934,5.934,0,0,0,.56.781c0,.007.011.012.016.018a6.01,6.01,0,0,0,2.518,1.737l.009,0a6,6,0,0,0,4.05,0l.009,0A6.01,6.01,0,0,0,14.552,10.9c.005-.006.011-.011.016-.018a5.934,5.934,0,0,0,.56-.781l.06-.1a6.061,6.061,0,0,0,.4-.836c.018-.044.034-.09.05-.135a5.968,5.968,0,0,0,.252-.908c.007-.038.011-.078.018-.116A6.108,6.108,0,0,0,16,7a6,6,0,0,0-6-6"
+            fill="currentColor"
+          />
+          <path
+            d="M10,14.084A7.4,7.4,0,0,1,8.941,14v3.942L10,19l1.059-1.058V14A7.4,7.4,0,0,1,10,14.084"
+            fill="currentColor"
+          />
         </g>
       </svg>
     </IconBox>
---
+++ src/icons/Semaphore.tsx
@@ -0,0 +1,18 @@
+import { IconBox } from '../elements/IconBox'
+
+import type { IconProps } from '../types'
+
+export function Semaphore({ color, size, ...nativeProps }: IconProps) {
+  return (
+    <IconBox $color={color} $size={size}>
+      <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
+        <path d="M0,0H20V20H0Z" fill="none" />
+        <path d="M0,0H20V20H0Z" fill="none" />
+        <path
+          d="M18,1H2V5H5L4,19H7V15h3v4h6L15,5h3ZM5,3.5H3v-2H5Zm3,0H6v-2H8Zm3,0H9v-2h2Zm3,0H12v-2h2Zm3,0H15v-2h2Z"
+          fill="currentColor"
+        />
+      </svg>
+    </IconBox>
+  )
+}
