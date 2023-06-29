ee8b9c6f104d38f6b8c7de0291dad3ed76f3173c
Loup Theron <loup.theron@gmail.com>
2023-06-29 10:09:33 +0200

    feat(fields): remove precision on DMD minute decimals

--- e2e/base/fields/CoordinatesInput.spec.tsx
+++ e2e/base/fields/CoordinatesInput.spec.tsx
@@ -1,27 +1,46 @@
 import { GlobalDecoratorWrapper } from '../../../.storybook/components/GlobalDecorator'
-import { CoordinatesFormat } from '../../../src'
-import type { CoordinatesInputProps } from '../../../src'
+import { CoordinatesFormat, CoordinatesInputProps } from '../../../src'
 import { _CoordinatesInput as CoordinatesInputStory } from '../../../stories/fields/CoordinatesInput.stories'
 import { mountAndWait, outputShouldBe, outputShouldNotBe } from '../utils'
 
 context('Story', () => {
-  const commonProps: CoordinatesInputProps = {
-    coordinatesFormat: CoordinatesFormat.DEGREES_MINUTES_DECIMALS,
-    label: 'Coordinates'
-  }
+  it('Should fill the DMD coordinates with lowercase N/W and two minutes decimals', () => {
+    const props: CoordinatesInputProps = {
+      coordinatesFormat: CoordinatesFormat.DEGREES_MINUTES_DECIMALS,
+      label: 'Coordinates'
+    }
 
-  it('Should fill the DMD coordinates with lowercase N/W', () => {
     mountAndWait(
       <GlobalDecoratorWrapper>
-        <CoordinatesInputStory {...commonProps} />
+        <CoordinatesInputStory {...props} />
       </GlobalDecoratorWrapper>
     )
 
     outputShouldNotBe()
 
-    cy.getDataCy('dmd-coordinates-input').type('4754456n00211545w')
+    cy.getDataCy('dmd-coordinates-input').type('025916S0172486w')
     cy.get('.Field-CoordinatesInput').should('not.contain', 'La latitude doit être N ou S')
 
-    outputShouldBe([47.9076, -2.192417])
+    outputShouldBe([-2.986, -17.414333])
+  })
+
+  it('Should fill the DMS coordinates with lowercase N/W', () => {
+    const props: CoordinatesInputProps = {
+      coordinatesFormat: CoordinatesFormat.DEGREES_MINUTES_SECONDS,
+      label: 'Coordinates'
+    }
+
+    mountAndWait(
+      <GlobalDecoratorWrapper>
+        <CoordinatesInputStory {...props} />
+      </GlobalDecoratorWrapper>
+    )
+
+    outputShouldNotBe()
+
+    cy.getDataCy('dms-coordinates-input').type('025910S0172452w')
+    cy.get('.Field-CoordinatesInput').should('not.contain', 'La latitude doit être N ou S')
+
+    outputShouldBe([-2.986111, -17.414444])
   })
 })
