180224ea7e0d3c5428f100d4d2c32b2d96bf31e3
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-07-14 12:19:34 +0200

    ci(eslint): forbid skipped & exclusive e2e tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ const BOOLEAN_CAMEL_PREFIXES = [
 const BOOLEAN_UPPER_PREFIXES = ['CAN_', 'DID_', 'IS_', 'HAD_', 'HAS_', 'MUST_', 'SHOULD_', 'WAS_', 'WILL_', 'WITH_']
 
 module.exports = {
-  extends: '@ivangabriele/eslint-config-typescript-react',
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
+  plugins: ['prettier', 'sort-keys-fix', 'sort-destructure-keys', 'typescript-sort-keys', 'no-null'],
   env: {
     browser: true,
     node: false
@@ -30,8 +31,11 @@ module.exports = {
     'rollup.config.js'
   ],
   rules: {
+    curly: ['error', 'all'],
+    'newline-before-return': 'error',
     'no-console': ['error', { allow: ['error', 'warn'] }],
 
+    '@typescript-eslint/lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
     // We must add PascalCase in formats because ESLint trim the prefix before evaluating the case
     // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/naming-convention.md#format-options
     // > Note: As documented above, the prefix is trimmed before format is validated,
@@ -99,9 +103,7 @@ module.exports = {
         }
       }
     ],
-
-    '@typescript/no-use-before-define': 'off',
-
+    '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
@@ -111,20 +113,43 @@ module.exports = {
     ],
     '@typescript-eslint/no-use-before-define': 'off',
 
+    'import/no-default-export': 'error',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true, peerDependencies: true }],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc'
+        },
+        groups: [['builtin', 'external', 'internal'], ['parent', 'index', 'sibling'], ['type'], ['object']],
+        'newlines-between': 'always'
+      }
+    ],
+    'import/prefer-default-export': 'off',
 
     'no-null/no-null': 'error',
 
+    'prettier/prettier': 'error',
+
     'react/jsx-no-useless-fragment': 'off',
     'react/jsx-props-no-spreading': 'off',
-    // See https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/default_props/#you-may-not-need-defaultprops
+    'react/jsx-sort-props': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 'off',
-    'react/react-in-jsx-scope': 'off'
+
+    'sort-destructure-keys/sort-destructure-keys': ['error', { caseSensitive: false }],
+
+    'sort-keys-fix/sort-keys-fix': ['error', 'asc', { caseSensitive: false, natural: false }],
+
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'error'
   },
   overrides: [
     {
-      files: ['./e2e/**/*.js', './e2e/**/*.ts', './e2e/**/*.tsx'],
-      plugins: ['cypress'],
+      files: ['./e2e/**/*.ts', './e2e/**/*.tsx'],
+      plugins: ['cypress', 'mocha'],
       env: {
         browser: false,
         node: true
@@ -137,17 +162,30 @@ module.exports = {
         'cypress/assertion-before-screenshot': 'error',
         'cypress/no-force': 'error',
         'cypress/no-async-tests': 'error',
-        'cypress/no-pause': 'error'
+        'cypress/no-pause': 'error',
+
+        'mocha/no-exclusive-tests': 'error',
+        'mocha/no-skipped-tests': 'error'
       }
     },
     {
       files: ['./src/**/*.test.ts', './src/**/*.test.tsx'],
+      plugins: ['jest'],
       env: {
         browser: true,
+        jest: true,
         node: true
       },
       rules: {
-        '@typescript-eslint/naming-convention': 'off'
+        '@typescript-eslint/naming-convention': 'off',
+
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+
+        'jest/no-disabled-tests': 'error',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/valid-expect': 'error'
       }
     },
     {
@@ -158,7 +196,9 @@ module.exports = {
         node: true
       },
       rules: {
-        '@typescript-eslint/naming-convention': 'off'
+        '@typescript-eslint/naming-convention': 'off',
+
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
       }
     },
     {
