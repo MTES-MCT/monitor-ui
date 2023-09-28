0fdc61b7b49231ed2d68d05b338dac4d08706180
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-09-28 03:22:23 +0200

    refactor(libs): split CustomSearch code into multiple files

---
+++ src/libs/CustomSearch/types.ts
@@ -0,0 +1,47 @@
+import type Fuse from 'fuse.js'
+
+export type CustomSearchKey<T> = string | Fuse.FuseOptionKeyObject<T>
+
+export type CustomSearchOptions = Partial<{
+  /** Cache search index to avoid Must be unique in the entire application. */
+  cacheKey: string | undefined
+  /**
+   * Indicates whether comparisons should be case sensitive.
+   *
+   * @default false
+   * @see https://fusejs.io/api/options.html#iscasesensitive
+   */
+  isCaseSensitive: boolean
+  /** Indicates whether comparisons should be diacritic (= accent) sensitive. */
+  isDiacriticSensitive: boolean
+  /**
+   * Use strict keywords matching, disabling fuzziness.
+   *
+   * @default false
+   * @description
+   * Looking for "emoi" in ["mÉMOIre", "mÉMOrIsable"] will return only the first one if `isStrict` is `true`,
+   * instead of returning both by default (`false`).
+   */
+  isStrict: boolean
+  /**
+   * By default, location is set to 0 and distance to 100
+   * When isStrict is false, for something to be considered a match, it would have to be within
+   * (threshold) 0.4 x (distance) 100 = 40 characters away from the expected location 0. (i.e. the first 40 characters)
+   * When true, search will ignore location and distance, so it won't matter where in the string the pattern appears.
+   *
+   * @default true
+   * @see https://www.fusejs.io/concepts/scoring-theory.html#scoring-theory
+   */
+  shouldIgnoreLocation: boolean
+  /**
+   * At what point does the match algorithm give up.
+   *
+   * @default 0.4
+   * @description
+   * A threshold of `0.0` requires a perfect match (of both letters and location),
+   * a threshold of `1.0` would match anything.
+   *
+   * @see https://fusejs.io/api/options.html#threshold
+   */
+  threshold: number
+}>
---
+++ src/libs/CustomSearch/utils/cleanCollectionDiacritics.ts
@@ -0,0 +1,58 @@
+import diacritics from 'diacritics'
+import { flow, get, update } from 'lodash/fp'
+
+import type { CustomSearchKey } from '../types'
+
+/**
+ * Replace all the diacritics with unaccented letters in a collection, targetting the provided keys.
+ *
+ * @param   collection  An array of objects
+ * @param   keys        List of item prop path to clean, either a using strings
+ *                      or [Fuse.js weighted objects](https://fusejs.io/api/options.html#keys)
+ * @returns             The same collection with its specified keys normalized without diacritics
+ *
+ * @example
+ * ```ts
+ * const collection = [
+ *   { name: 'aérosol', description: 'Un aérosol.', author: { name: 'Camille Hervé' } },
+ *   { name: 'martèlement', description: 'Un martèlement.', author: { name: 'Athénée Perreault' } }
+ * ]
+ *
+ * const normalizedCollection = cleanCollectionDiacritics(collection, ['name', 'author.name'])
+ * console.log(normalizedCollection[0])
+ * // => `{ "name": "aerosol", "description": "Un aérosol.", author: { name: 'Camille Herve' } }`
+ *
+ * const normalizedCollection = cleanCollectionDiacritics(collection, [{ name: 'name', weight: 1 }])
+ * console.log(normalizedCollection[0])
+ * // => `{ "name": "aerosol", "description": "Un aérosol.", author: { name: 'Camille Hervé' }`
+ * ```
+ */
+export function cleanCollectionDiacritics<T extends Record<string, any> = Record<string, any>>(
+  collection: T[],
+  keys: Array<CustomSearchKey<T>>
+): T[] {
+  // These are the prop paths that needs to be normalized,
+  // represented as an array dot-seperated strings (i.e.: `['aProperty', 'a.nested.property']`)
+  const collectionKeysAsPaths = keys.map(key => (typeof key === 'string' ? key : key.name))
+
+  // Now that we have a list of prop paths, we want to generate a list of transformers
+  // able for remove diacritics of a collection item, one transformer per collection item prop path.
+  const collectionItemTransformers = collectionKeysAsPaths.map(collectionKeyAsPath => (collectionItem: T): T => {
+    const collectionItemPropValue = get(collectionKeyAsPath, collectionItem)
+
+    // This type check is a safeguard against unexpected values (a non-string value would otherwise throw an error)
+    if (typeof get(collectionKeyAsPath, collectionItem) !== 'string') {
+      console.warn(`"${collectionKeyAsPath}" is not a string (value: \`${collectionItemPropValue}\`).`)
+
+      return collectionItem
+    }
+
+    return update(collectionKeyAsPath, diacritics.remove, collectionItem)
+  })
+
+  // We can then generate a single function piping all these collection item transformers...
+  const cleanCollectionItemDiacritics: (collectionItem: T) => T = flow(collectionItemTransformers)
+
+  // ...and apply them to each collection item via a mapper
+  return collection.map(cleanCollectionItemDiacritics)
+}
