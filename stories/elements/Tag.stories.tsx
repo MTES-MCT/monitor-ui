f58b3f42746286b9a27b04018101e4cd2f2263f6
Claire Dagan <clairedagan@gmail.com>
2023-10-02 14:27:34 +0200

    fix(elements): add backgroundColor prop and fix icon size in Tag component

--- stories/elements/Tag.stories.tsx
+++ stories/elements/Tag.stories.tsx
@@ -1,6 +1,8 @@
+import styled from 'styled-components'
+
 import { generateStoryDecorator } from '../../.storybook/components/StoryDecorator'
 import { getEnumValuesWithUndefined, getUndefinedPropsFromUndefinedStringProps } from '../../.storybook/utils'
-import { Accent, Tag, TagBullet } from '../../src'
+import { Accent, Icon, THEME, Tag, TagBullet } from '../../src'
 
 import type { TagProps } from '../../src'
 import type { Meta } from '@storybook/react'
@@ -25,6 +27,12 @@ const meta: Meta<TagProps> = {
     },
     bulletColor: {
       control: 'color'
+    },
+    color: {
+      control: 'color'
+    },
+    backgroundColor: {
+      control: 'color'
     }
   },
 
@@ -43,8 +51,33 @@ export function _Tag(props: TagProps) {
   const normalizedProps = getUndefinedPropsFromUndefinedStringProps(props)
 
   return (
-    <>
-      <Tag {...normalizedProps} />
-    </>
+    <TagsContainer>
+      <Tag accent={Accent.PRIMARY} {...normalizedProps}>
+        A primary tag
+      </Tag>
+      <Tag accent={Accent.SECONDARY} {...normalizedProps}>
+        A secondary tag
+      </Tag>
+      <Tag accent={Accent.TERTIARY} {...normalizedProps}>
+        A tertiary tag
+      </Tag>
+      <Tag bullet={TagBullet.DISK} bulletColor={THEME.color.mediumSeaGreen} {...normalizedProps}>
+        A tag with a green bullet
+      </Tag>
+      <Tag
+        backgroundColor={THEME.color.maximumRed15}
+        color={THEME.color.charcoal}
+        Icon={Icon.Link}
+        {...normalizedProps}
+      >
+        A tag with custom colors and icon
+      </Tag>
+    </TagsContainer>
   )
 }
+
+const TagsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`
