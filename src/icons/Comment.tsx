2aca64e1a017454c555d860d3a78bb6ae87fab77
lhache <louis.hache.dev@protonmail.com>
2023-11-14 17:37:12 +0100

    fix(icons): update icon components after svg change

--- src/icons/Comment.tsx
+++ src/icons/Comment.tsx
@@ -10,7 +10,7 @@ export function Comment({ color, size, ...nativeProps }: IconProps) {
           <path d="m0 0h20v20h-20z" />
         </clipPath>
         <g clipPath="url(#a)">
-          <path d="m0 0h20v20h-20z" fill="#fff" />
+          <path d="m0 0h20v20h-20z" fill="transparent" />
           <path
             d="m6.455 19.41-4.455 3.59v-18.974a1.013 1.013 0 0 1 1-1.026h18a1.013 1.013 0 0 1 1 1.026v14.359a1.013 1.013 0 0 1 -1 1.026zm.545-9.231v2.051h2v-2.051zm4 0v2.051h2v-2.051zm4 0v2.051h2v-2.051z"
             fill="currentColor"
