e97302b3b736abd7f92e8658c8a2e2a38e4d1b6d
Ivan Gabriele <ivan.gabriele@protonmail.com>
2023-11-22 10:17:02 +0100

    feat(entities): add Administration, ControlUnit, DepartmentArea & Station

---
+++ src/entities/Administration.ts
@@ -0,0 +1,17 @@
+import type { ControlUnit } from './ControlUnit'
+
+export namespace Administration {
+  export interface Administration {
+    controlUnitIds: number[]
+    controlUnits: ControlUnit.ControlUnitData[]
+    id: number
+    isArchived: boolean
+    name: string
+  }
+
+  // ---------------------------------------------------------------------------
+  // Types
+
+  export type AdministrationData = Omit<Administration, 'controlUnitIds' | 'controlUnits'>
+  export type NewAdministrationData = Omit<AdministrationData, 'id'>
+}
---
+++ src/entities/Mission.ts
@@ -0,0 +1,8 @@
+export namespace Mission {
+  export enum MissionSourceEnum {
+    MONITORENV = 'MONITORENV',
+    MONITORFISH = 'MONITORFISH',
+    POSEIDON_CACEM = 'POSEIDON_CACEM',
+    POSEIDON_CNSP = 'POSEIDON_CNSP'
+  }
+}
