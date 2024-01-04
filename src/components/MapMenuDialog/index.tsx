751b34003c28c9d4eb9e893c865a47971b6f4fcf
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-01-04 11:20:53 +0100

    fix(components): truncate long titles in MapMenuDialog

--- src/components/MapMenuDialog/index.tsx
+++ src/components/MapMenuDialog/index.tsx
@@ -3,28 +3,34 @@ import styled from 'styled-components'
 import { IconButton } from '../../elements/IconButton'
 
 const Container = styled.div`
-  width: 320px;
-  max-height: 520px;
-  margin-right: 6px;
   background-color: ${p => p.theme.color.white};
   box-shadow: 0px 3px 6px ${p => p.theme.color.slateGray};
   display: flex;
   flex-direction: column;
+  margin-right: 6px;
+  max-height: 520px;
+  width: 320px;
 `
 
 const Header = styled.div`
-  height: 40px;
+  align-items: center;
   background-color: ${p => p.theme.color.charcoal};
   display: flex;
+  height: 40px;
   justify-content: space-between;
   padding: 9px 4px 9px 10px;
-  align-items: center;
 `
 
-const Title = styled.span`
+const Title = styled.span.attrs(props => ({
+  title: String(props.children)
+}))`
+  color: ${p => p.theme.color.white};
   font-size: 16px;
   line-height: 22px;
-  color: ${p => p.theme.color.white};
+  margin-right: 6px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
 `
 
 const Body = styled.div`
