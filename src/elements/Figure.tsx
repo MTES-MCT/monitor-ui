0e5a3dc04df50bc1940607ed6b394a1ba6ffd010
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-01-17 16:14:22 +0100

    feat(elements): add Figure

---
+++ src/elements/Figure.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+import type { HTMLAttributes } from 'react'
+
+export type FigureProps = HTMLAttributes<HTMLSpanElement>
+
+/**
+ * <Figure /> uses the Open Sans font to render fixed-width digits.
+ */
+export const Figure = styled.span`
+  font-family: 'Open Sans Variable', monospace;
+`
