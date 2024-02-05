1d7cc3fc20716454ae4dbb80571959eb8e48f166
Claire Dagan <clairedagan@gmail.com>
2024-02-05 12:43:55 +0100

    feat(icons): add warning icon

---
+++ src/icons/Warning.tsx
@@ -0,0 +1,16 @@
+import { IconBox } from '../elements/IconBox'
+
+import type { IconProps } from '../types/definitions'
+
+export function Warning({ color, size, ...nativeProps }: IconProps) {
+  return (
+    <IconBox $color={color} $size={size}>
+      <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
+        <path d="M18,18H2l-.883-1.471,8-15h1.766l8,15ZM3.667,16H16.333L10,4.125Z" fill="currentColor" />
+        <rect fill="currentColor" height="4.011" transform="translate(9 7.989)" width="2" />
+        <rect fill="currentColor" height="2" transform="translate(9 13)" width="2" />
+        <path d="M0,0H20V20H0Z" fill="none" />
+      </svg>
+    </IconBox>
+  )
+}
