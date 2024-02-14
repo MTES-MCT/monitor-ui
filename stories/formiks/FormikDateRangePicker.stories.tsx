aa7cbeb64c6a2afbcc1c8daf179f42f60cc01dd4
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-14 16:32:58 +0100

    build: migrate from Webpack to Vite

--- stories/formiks/FormikDateRangePicker.stories.tsx
+++ stories/formiks/FormikDateRangePicker.stories.tsx
@@ -24,7 +24,7 @@ const args: FormikDateRangePickerWithDateDateProps | FormikDateRangePickerWithSt
   withTime: true
 }
 
-const meta: Meta<FormikDateRangePickerWithDateDateProps | FormikDateRangePickerWithStringDateProps> = {
+const meta: Meta<any> = {
   title: 'Formiks/FormikDateRangePicker',
   component: FormikDateRangePicker,
 
