7f17c2f35b9d0d0928cfed4b818a09558c8db183
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-15 00:28:40 +0100

    refactor(vite): remove commented code

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-// import svgr from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
