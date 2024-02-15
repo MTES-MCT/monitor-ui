25738f644014801e95d9d2102689cc73e498dd50
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-15 09:23:04 +0100

    build(storybook): move config to full TS

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
   addons: ['@storybook/addon-essentials', 'storybook-addon-performance', '@storybook/addon-a11y'],
   docs: {
     autodocs: 'tag'
@@ -8,7 +9,8 @@ const config = {
     name: '@storybook/react-vite',
     options: {}
   },
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(ts|tsx)']
+  stories: ['../stories/**/*.stories.@(mdx|ts|tsx)']
 }
 
+// eslint-disable-next-line import/no-default-export
 export default config
