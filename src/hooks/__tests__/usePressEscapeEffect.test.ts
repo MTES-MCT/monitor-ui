29a66b5b98ac771533495c82a771b55536052aec
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-19 02:17:42 +0100

    feat(hooks): add usePressEscapeEffect()

---
+++ src/hooks/__tests__/usePressEscapeEffect.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from '@jest/globals'
+import { renderHook } from '@testing-library/react'
+
+import { usePressEscapeEffect } from '../usePressEscapeEffect'
+
+describe('usePressEscapeEffect', () => {
+  const mockCallback = jest.fn()
+
+  it('calls callback when Escape key is pressed', () => {
+    renderHook(() => usePressEscapeEffect(mockCallback))
+
+    const escapeEvent = new KeyboardEvent('keydown', { key: 'Escape' })
+    document.dispatchEvent(escapeEvent)
+
+    expect(mockCallback).toHaveBeenCalledTimes(1)
+  })
+
+  it('does not call callback when another key is pressed', () => {
+    renderHook(() => usePressEscapeEffect(mockCallback))
+
+    const otherKeyEvent = new KeyboardEvent('keydown', { key: 'Enter' })
+    document.dispatchEvent(otherKeyEvent)
+
+    expect(mockCallback).not.toHaveBeenCalled()
+  })
+})
