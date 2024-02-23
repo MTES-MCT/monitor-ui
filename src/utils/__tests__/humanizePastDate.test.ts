8073a817581ae418e8dab7551ccc7024574293d6
Claire Dagan <clairedagan@gmail.com>
2024-02-23 13:27:13 +0100

    feat(utils): changes after code review

--- src/utils/__tests__/humanizePastDate.test.ts
+++ src/utils/__tests__/humanizePastDate.test.ts
@@ -1,31 +1,30 @@
 import { describe, expect, it } from '@jest/globals'
 import dayjs from 'dayjs'
 
-import { getFormattedMissionUpdateDate } from '../getFormattedMissionUpdateDate'
+import { humanizePastDate } from '../humanizePastDate'
 
 describe('utils/getFormattedMissionUpdateDate()', () => {
   it('should return the expected message when the date is today', () => {
     const missionUpdatedAtUtc = dayjs().subtract(3, 'hour').utc()
-    const result = getFormattedMissionUpdateDate(missionUpdatedAtUtc.format('YYYY-MM-DDTHH:mm:ssZ'))
+    const result = humanizePastDate(missionUpdatedAtUtc.format('YYYY-MM-DDTHH:mm:ssZ'))
 
-    const expectedHour = `${missionUpdatedAtUtc.format('HH')}h${missionUpdatedAtUtc.format('mm')}`
+    const expectedHour = `${missionUpdatedAtUtc.format('HH[h]mm')}`
     expect(result).toStrictEqual(`aujourdhui à ${expectedHour} (UTC)`)
   })
 
   it('should return the expected message when the date is yesterday', () => {
     const missionUpdatedAtUtc = dayjs().subtract(1, 'day').utc()
-    const result = getFormattedMissionUpdateDate(missionUpdatedAtUtc.format('YYYY-MM-DDTHH:mm:ssZ'))
+    const result = humanizePastDate(missionUpdatedAtUtc.format('YYYY-MM-DDTHH:mm:ssZ'))
 
-    const expectedHour = `${missionUpdatedAtUtc.format('HH')}h${missionUpdatedAtUtc.format('mm')}`
-
+    const expectedHour = `${missionUpdatedAtUtc.format('HH[h]mm')}`
     expect(result).toStrictEqual(`hier à ${expectedHour} (UTC)`)
   })
 
   it('should return the expected message when the date is neither today nor yesterday', () => {
     const missionUpdatedAtUtc = dayjs().subtract(3, 'day').utc()
-    const result = getFormattedMissionUpdateDate(missionUpdatedAtUtc.format('YYYY-MM-DDTHH:mm:ssZ'))
+    const result = humanizePastDate(missionUpdatedAtUtc.format('YYYY-MM-DDTHH:mm:ssZ'))
 
-    const expectedHour = `${missionUpdatedAtUtc.format('HH')}h${missionUpdatedAtUtc.format('mm')}`
+    const expectedHour = `${missionUpdatedAtUtc.format('HH[h]mm')}`
     expect(result).toStrictEqual(`le ${missionUpdatedAtUtc.format('DD/MM/YYYY')} à ${expectedHour} (UTC)`)
   })
 })
