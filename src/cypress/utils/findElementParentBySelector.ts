a2dc37935989c54f4151d5fc47fde6956707cdf0
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-28 03:13:16 +0100

    fix(cypress): fix inner findElementParentBySelector() util

--- src/cypress/utils/findElementParentBySelector.ts
+++ src/cypress/utils/findElementParentBySelector.ts
@@ -5,19 +5,20 @@ export function findElementParentBySelector<T extends HTMLElement = HTMLElement>
   parentSelector: string,
   index: number = 0
 ): T | undefined {
+  let currentParentElement: HTMLElement | null = element
   let lastFoundParentIndex: number = 0
   let parentIndex = -1
   while (parentIndex < MAX_PARENT_INDEX) {
     parentIndex += 1
 
-    const { parentElement } = element
-    if (!parentElement) {
+    currentParentElement = currentParentElement.parentElement
+    if (!currentParentElement) {
       return undefined
     }
 
-    if (parentElement.matches(parentSelector)) {
+    if (currentParentElement.matches(parentSelector)) {
       if (lastFoundParentIndex === index) {
-        return parentElement as T
+        return currentParentElement as T
       }
 
       lastFoundParentIndex += 1
