d75171ab811bd7cbebd61efa617b4d67c2ea874b
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-29 01:54:34 +0100

    feat(cypress): add forceUncheck() command

---
+++ src/cypress/commands/forceUncheck.ts
@@ -0,0 +1,21 @@
+export function forceUncheck<Subject = any>(
+  this: Mocha.Context,
+  subject: Subject,
+  options: Partial<Cypress.CheckOptions> = {}
+): Cypress.Chainable<Subject> {
+  Cypress.log({
+    consoleProps: () => ({
+      'Applied to': subject,
+      Elements: 1
+    }),
+    name: 'forceUncheck'
+  })
+
+  if (!subject) {
+    throw new Error(`Could not find subject.`)
+  }
+
+  const wrappedSubject = cy.wrap<Subject>(subject)
+
+  return wrappedSubject.uncheck({ ...options, force: true })
+}
--- src/cypress/index.ts
+++ src/cypress/index.ts
@@ -5,6 +5,7 @@ import { fill } from './commands/fill'
 import { forceCheck } from './commands/forceCheck'
 import { forceClick } from './commands/forceClick'
 import { forceType } from './commands/forceType'
+import { forceUncheck } from './commands/forceUncheck'
 import { getDataCy } from './commands/getDataCy'
 import { getTableRowById } from './commands/getTableRowById'
 import { getTableRowByText } from './commands/getTableRowByText'
@@ -19,6 +20,7 @@ export const registerMonitorUiCustomCommands = () => {
   Cypress.Commands.add('forceCheck', { prevSubject: true }, forceCheck)
   Cypress.Commands.add('forceClick', { prevSubject: true }, forceClick)
   Cypress.Commands.add('forceType', { prevSubject: true }, forceType)
+  Cypress.Commands.add('forceUncheck', { prevSubject: true }, forceUncheck)
   Cypress.Commands.add('getDataCy', getDataCy)
   Cypress.Commands.add('getTableRowById', { prevSubject: 'optional' } as any, getTableRowById)
   Cypress.Commands.add('getTableRowByText', { prevSubject: 'optional' } as any, getTableRowByText)
