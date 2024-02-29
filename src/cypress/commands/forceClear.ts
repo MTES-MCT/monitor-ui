ebd5ad86249aaf333ce088b06561b5827bd8a156
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-29 01:55:44 +0100

    feat(cypress): add forceClear() command

---
+++ src/cypress/commands/forceClear.ts
@@ -0,0 +1,21 @@
+export function forceClear<Subject = any>(
+  this: Mocha.Context,
+  subject: Subject,
+  options: Partial<Cypress.ClearOptions> = {}
+): Cypress.Chainable<Subject> {
+  Cypress.log({
+    consoleProps: () => ({
+      'Applied to': subject,
+      Elements: 1
+    }),
+    name: 'forceClear'
+  })
+
+  if (!subject) {
+    throw new Error(`Could not find subject.`)
+  }
+
+  const wrappedSubject = cy.wrap<Subject>(subject)
+
+  return wrappedSubject.clear({ ...options, force: true })
+}
