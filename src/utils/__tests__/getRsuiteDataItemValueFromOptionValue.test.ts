9c0732e0e61af88ab0c17d100f37c8169d89de24
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-03-01 00:42:28 +0100

    feat(utils): add getSelectedOptionFromOptionValue()

--- src/utils/__tests__/getRsuiteDataItemValueFromOptionValue.test.ts
+++ src/utils/__tests__/getRsuiteDataItemValueFromOptionValue.test.ts
@@ -3,15 +3,15 @@ import { describe, expect, it } from '@jest/globals'
 import { getRsuiteDataItemValueFromOptionValue } from '../getRsuiteDataItemValueFromOptionValue'
 
 describe('utils/getRsuiteDataItemValueFromOptionValue()', () => {
-  it('should convert string values without <optionValueKey>', () => {
-    const optionValue = 'Test'
+  it('should convert a boolean option value without <optionValueKey>', () => {
+    const optionValue = true
 
     const result = getRsuiteDataItemValueFromOptionValue(optionValue)
 
-    expect(result).toBe('Test')
+    expect(result).toBe('true')
   })
 
-  it('should convert number values without <optionValueKey>', () => {
+  it('should convert a number option value without <optionValueKey>', () => {
     const optionValue = 123
 
     const result = getRsuiteDataItemValueFromOptionValue(optionValue)
@@ -19,7 +19,15 @@ describe('utils/getRsuiteDataItemValueFromOptionValue()', () => {
     expect(result).toBe('123')
   })
 
-  it('should convert object values with <optionValueKey>', () => {
+  it('should convert a string option value without <optionValueKey>', () => {
+    const optionValue = 'Test'
+
+    const result = getRsuiteDataItemValueFromOptionValue(optionValue)
+
+    expect(result).toBe('Test')
+  })
+
+  it('should convert an object value with <optionValueKey>', () => {
     const optionValue = { id: 1, name: 'Test Name' }
 
     const result = getRsuiteDataItemValueFromOptionValue(optionValue, 'name')
@@ -27,7 +35,7 @@ describe('utils/getRsuiteDataItemValueFromOptionValue()', () => {
     expect(result).toBe('Test Name')
   })
 
-  it('should throw an error for object values without <optionValueKey>', () => {
+  it('should throw an error for an object option value without <optionValueKey>', () => {
     const optionValue = { id: 1, name: 'Test Name' }
 
     const call = () => getRsuiteDataItemValueFromOptionValue(optionValue)
@@ -35,7 +43,7 @@ describe('utils/getRsuiteDataItemValueFromOptionValue()', () => {
     expect(call).toThrow()
   })
 
-  it('should throw an error for object values with an invalid <optionValueKey>', () => {
+  it('should throw an error for an object option value with <optionValueKey> when it does NOT exist', () => {
     const optionValue = { id: 1, name: 'Test Name' }
 
     const call = () => getRsuiteDataItemValueFromOptionValue(optionValue)
