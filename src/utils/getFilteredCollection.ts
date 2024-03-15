58fb2edf4f0d136c0587b4bbc0fc87e043e63bb9
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-03-15 03:04:36 +0100

    fix(utils): remove conditional types from getFilteredCollection()

--- src/utils/getFilteredCollection.ts
+++ src/utils/getFilteredCollection.ts
@@ -1,7 +1,5 @@
 import type { Filter } from '../types/definitions'
 
-export function getFilteredCollection<T>(collection: undefined, filters: Array<Filter<T>>): undefined
-export function getFilteredCollection<T>(collection: T[], filters: Array<Filter<T>>): T[]
 export function getFilteredCollection<T>(collection: T[] | undefined, filters: Array<Filter<T>>): T[] | undefined {
   if (collection === undefined) {
     return undefined
