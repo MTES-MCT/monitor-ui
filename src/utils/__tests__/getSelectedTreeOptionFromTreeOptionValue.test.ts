757facfd397aa5b06f4cf99565ac00b24876470b
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-03-21 14:25:55 +0100

    test(utils): use inline values rather than references for expected values

--- src/utils/__tests__/getSelectedTreeOptionFromTreeOptionValue.test.ts
+++ src/utils/__tests__/getSelectedTreeOptionFromTreeOptionValue.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from '@jest/globals'
 
 // Assuming these mocks are structured similarly to the ones used previously,
 // adjusted for tree options
+import { LOREM_IPSUM } from '../../../__mocks__/fake_text'
 import {
   FAKE_NUMBER_TREE_OPTIONS,
   FAKE_OBJECT_TREE_OPTIONS,
@@ -21,35 +22,35 @@ describe('utils/getSelectedTreeOptionFromTreeOptionValue()', () => {
 
   it('should return the correct tree option for number values', () => {
     const allOptions = FAKE_NUMBER_TREE_OPTIONS
-    const selectedOptionValue = FAKE_NUMBER_TREE_OPTIONS[1]!.children![1]!.value
+    const selectedOptionValue = 4
 
     const result = getSelectedTreeOptionFromTreeOptionValue(allOptions, selectedOptionValue)
 
-    expect(result).toBe(FAKE_NUMBER_TREE_OPTIONS[1]!.children![1]!)
+    expect(result).toEqual({ label: LOREM_IPSUM, value: 4 })
   })
 
   it('should return the correct tree option for object values', () => {
     const allOptions = FAKE_OBJECT_TREE_OPTIONS
-    const selectedOptionValue = FAKE_OBJECT_TREE_OPTIONS[1]!.children![1]!.value
+    const selectedOptionValue = { id: 4, name: 'Chatty Option Name' }
     const optionValueKey = 'id'
 
     const result = getSelectedTreeOptionFromTreeOptionValue(allOptions, selectedOptionValue, optionValueKey)
 
-    expect(result).toBe(FAKE_OBJECT_TREE_OPTIONS[1]!.children![1]!)
+    expect(result).toEqual({ label: LOREM_IPSUM, value: { id: 4, name: 'Chatty Option Name' } })
   })
 
   it('should return the correct tree option for string values', () => {
     const allOptions = FAKE_STRING_TREE_OPTIONS
-    const selectedOptionValue = FAKE_STRING_TREE_OPTIONS[1]!.children![1]!.value
+    const selectedOptionValue = 'CHATTY_OPTION'
 
     const result = getSelectedTreeOptionFromTreeOptionValue(allOptions, selectedOptionValue)
 
-    expect(result).toBe(FAKE_STRING_TREE_OPTIONS[1]!.children![1]!)
+    expect(result).toEqual({ label: LOREM_IPSUM, value: 'CHATTY_OPTION' })
   })
 
   it('should throw an error with object option when <selectedOptionValue> does NOT exist', () => {
     const allOptions = FAKE_OBJECT_TREE_OPTIONS
-    const selectedOptionValue = FAKE_OBJECT_TREE_OPTIONS[1]!.children![1]!.value
+    const selectedOptionValue = { id: 4, name: 'Chatty Option Name' }
     const optionValueKey = 'non-existent'
 
     const call = () => getSelectedTreeOptionFromTreeOptionValue(allOptions, selectedOptionValue, optionValueKey as any)
