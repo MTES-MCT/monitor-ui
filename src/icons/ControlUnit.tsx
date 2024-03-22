a33fb25af254e72ffd317560f2e07127d2396c34
Claire Dagan <clairedagan@gmail.com>
2024-03-22 15:49:15 +0100

    fix(icons): fix controlUnit and controlUnitFilled icons

--- src/icons/ControlUnit.tsx
+++ src/icons/ControlUnit.tsx
@@ -7,19 +7,16 @@ export function ControlUnit({ color, size, title, ...nativeProps }: IconProps) {
     <IconBox $color={color} $size={size} title={title}>
       <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
         <g transform="translate(80 205)">
-          <g>
-            <rect fill="currentColor" height="12" transform="translate(-64 -188) rotate(90)" width="2" />
-            <path
-              d="M-67.694-199l1.333,8h-7.277l1.336-8Zm0-2h-4.6a2,2,0,0,0-1.977,1.675L-76-189h12l-1.721-10.325A2,2,0,0,0-67.7-201Z"
-              fill="currentColor"
-            />
-            <rect fill="currentColor" height="2.25" transform="translate(-70.75 -204.25)" width="1.5" />
-            <rect fill="currentColor" height="2.25" transform="translate(-64.061 -203) rotate(45)" width="1.5" />
-            <rect fill="currentColor" height="2.25" transform="translate(-77 -201.939) rotate(-45)" width="1.5" />
-          </g>
-          <rect fill="none" height="20" transform="translate(-80 -205)" width="20" />
-          <path d="M-80-205h20v20H-80Z" fill="none" />
+          <rect fill="currentColor" height="12" transform="translate(-64 -188) rotate(90)" width="2" />
+          <path
+            d="M-67.694-199l1.333,8h-7.277l1.336-8Zm0-2h-4.6a2,2,0,0,0-1.977,1.675L-76-189h12l-1.721-10.325A2,2,0,0,0-67.7-201Z"
+            fill="currentColor"
+          />
+          <rect fill="currentColor" height="2.25" transform="translate(-70.75 -204.25)" width="1.5" />
+          <rect fill="currentColor" height="2.25" transform="translate(-64.061 -203) rotate(45)" width="1.5" />
+          <rect fill="currentColor" height="2.25" transform="translate(-77 -201.939) rotate(-45)" width="1.5" />
         </g>
+        <rect fill="none" height="20" width="20" />
       </svg>
     </IconBox>
   )
--- src/icons/ControlUnitFilled.tsx
+++ src/icons/ControlUnitFilled.tsx
@@ -5,8 +5,8 @@ import type { IconProps } from '../types/definitions'
 export function ControlUnitFilled({ color, size, title, ...nativeProps }: IconProps) {
   return (
     <IconBox $color={color} $size={size} title={title}>
-      <svg height="18.25" viewBox="0 0 14 18.25" width="14" {...nativeProps}>
-        <g transform="translate(77 204.25)">
+      <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
+        <g transform="translate(80 205)">
           <rect fill="currentColor" height="12" transform="translate(-64 -188) rotate(90)" width="2" />
           <path
             d="M-67.7-201h-4.6a2,2,0,0,0-1.977,1.675L-76-189h12l-1.721-10.325A2,2,0,0,0-67.7-201Z"
@@ -16,6 +16,7 @@ export function ControlUnitFilled({ color, size, title, ...nativeProps }: IconPr
           <rect fill="currentColor" height="2.25" transform="translate(-64.061 -203) rotate(45)" width="1.5" />
           <rect fill="currentColor" height="2.25" transform="translate(-77 -201.939) rotate(-45)" width="1.5" />
         </g>
+        <rect fill="none" height="20" width="20" />
       </svg>
     </IconBox>
   )
