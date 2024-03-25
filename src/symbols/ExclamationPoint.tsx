c5d90ef4d208eeba9d52c5cb034b1d70693602d0
lwih <louis.hache@beta.gouv.fr>
2024-03-25 11:58:32 +0100

    feat(symbols): rework ExclamationPoint to resize dynamically

--- src/symbols/ExclamationPoint.tsx
+++ src/symbols/ExclamationPoint.tsx
@@ -6,18 +6,22 @@ import type { HTMLAttributes } from 'react'
 export type ExclamationPointProps = HTMLAttributes<HTMLSpanElement> & {
   backgroundColor?: string | undefined
   color?: string | undefined
+  size?: number
 }
+
+const DEFAULT_SIZE = 20
+
 export const ExclamationPoint = styled.span<ExclamationPointProps>`
   background: ${p => p.backgroundColor ?? p.theme.color.goldenPoppy};
-  border-radius: 15px;
+  border-radius: ${p => `${p.size ?? DEFAULT_SIZE}px`};
   color: ${p => p.color ?? p.theme.color.white};
-  display: inline-block;
-  font-size: 11px;
+  display: inline-flex; /* use flexbox for easier centering */
+  justify-content: center; /* center content horizontally */
+  align-items: center; /* center content vertically */
+  font-size: ${p => `${p.size && p.size <= 13 ? p.size : 13}px`};
   font-weight: 700;
-  line-height: 11px !important;
-  height: 20px;
-  padding: 3.25px 4px 5px 8.25px;
-  width: 20px;
+  height: ${p => `${p.size ?? DEFAULT_SIZE}px`};
+  width: ${p => `${p.size ?? DEFAULT_SIZE}px`};
   box-sizing: border-box;
 
   ::after {
