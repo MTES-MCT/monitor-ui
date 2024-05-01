c47a237a53ee6f874ef4065c0dc947044e6cd0a7
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-05-01 10:06:55 +0200

    feat(elements): add Link

---
+++ src/elements/Link.tsx
@@ -0,0 +1,31 @@
+import { type AnchorHTMLAttributes } from 'react'
+import styled from 'styled-components'
+
+export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement>
+
+export const Link = styled.a<LinkProps>`
+  color: ${p => p.theme.color.slateGray};
+  text-decoration: underline;
+
+  &:hover,
+  &._hover {
+    color: ${p => p.theme.color.blueYonder};
+    svg {
+      color: ${p => p.theme.color.blueYonder};
+    }
+  }
+
+  &:active,
+  &._active {
+    color: ${p => p.theme.color.blueGray};
+    svg {
+      color: ${p => p.theme.color.blueGray};
+    }
+  }
+
+  &:visited {
+    color: ${p => p.theme.color.slateGray};
+  }
+`
+
+Link.displayName = 'LinkButton'
