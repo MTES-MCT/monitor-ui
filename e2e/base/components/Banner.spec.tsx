f4eacbe90f2cba08329c0d9c0a8ee6c92b807d07
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-05-15 17:46:21 +0200

    fix(components): fix onAutoClose prop callback in Banner

--- e2e/base/components/Banner.spec.tsx
+++ e2e/base/components/Banner.spec.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
+
+import { Output } from '../../../.storybook/components/Output'
 import { StoryBox } from '../../../.storybook/components/StoryBox'
-import { Level } from '../../../src'
+import { Banner, Level, type BannerProps } from '../../../src'
 import { _Banner as BannerStory } from '../../../stories/components/Banner.stories'
-import { mountAndWait } from '../utils'
+import { mountAndWait, outputShouldBe } from '../utils'
 
 context(`Story`, () => {
   describe('default visibility', () => {
@@ -13,8 +16,10 @@ context(`Story`, () => {
           </BannerStory>
         </StoryBox>
       )
+
       cy.get('.Banner-Storie').should('not.be.visible')
     })
+
     it('should show the banner when isHiddenByDefault is false', () => {
       mountAndWait(
         <StoryBox>
@@ -23,8 +28,10 @@ context(`Story`, () => {
           </BannerStory>
         </StoryBox>
       )
+
       cy.get('.Banner-Storie').should('be.visible')
     })
+
     it('should show the banner when isHiddenByDefault is undefined', () => {
       mountAndWait(
         <StoryBox>
@@ -33,9 +40,11 @@ context(`Story`, () => {
           </BannerStory>
         </StoryBox>
       )
+
       cy.get('.Banner-Storie').should('be.visible')
     })
   })
+
   describe('closable version', () => {
     it('should disappear when the action button is clicked', () => {
       mountAndWait(
@@ -45,10 +54,12 @@ context(`Story`, () => {
           </BannerStory>
         </StoryBox>
       )
+
       cy.get('.banner-button').first().click()
       cy.get('.Banner-Storie').should('not.be.visible')
     })
   })
+
   describe('collapsible version', () => {
     it('should collapse when the action button is clicked', () => {
       mountAndWait(
@@ -58,12 +69,93 @@ context(`Story`, () => {
           </BannerStory>
         </StoryBox>
       )
+
       // check it's fully opened
       cy.get('.Banner-Storie').invoke('outerHeight').should('be.gt', 10)
       // click on the hide button
       cy.get('.banner-button').first().click()
       // check it's shrunk
       cy.get('.Banner-Storie').invoke('outerHeight').should('be.equal', 10)
+    })
+  })
+})
+
+function BaseStory(props: Partial<BannerProps>) {
+  const [hasAutoClosed, setHasAutoClosed] = useState<boolean>(false)
+  const [hasClosed, setHasClosed] = useState<boolean>(false)
+
+  return (
+    <StoryBox>
+      <Banner
+        level={Level.SUCCESS}
+        onAutoClose={() => setHasAutoClosed(true)}
+        onClose={() => setHasClosed(true)}
+        top="0"
+        {...props}
+      >
+        some text
+      </Banner>
+
+      <Output
+        value={{
+          hasAutoClosed,
+          hasClosed
+        }}
+      />
+    </StoryBox>
+  )
+}
+
+context(`Base`, () => {
+  describe('with `closingDelay`, `isClosable` and `withAutomaticClosing`', () => {
+    it('should call both `onAutoClose()` and `onClose()` when waiting for auto-closing', () => {
+      mountAndWait(<BaseStory closingDelay={250} isClosable withAutomaticClosing />)
+
+      cy.get('.Component-Banner').should('be.empty')
+
+      outputShouldBe({
+        hasAutoClosed: true,
+        hasClosed: true
+      })
+    })
+
+    it('should only call `onClose()` when clicking on closing button', () => {
+      mountAndWait(<BaseStory closingDelay={99999} isClosable withAutomaticClosing />)
+
+      cy.clickButton('Fermer')
+
+      cy.get('.Component-Banner').should('be.empty')
+
+      outputShouldBe({
+        hasAutoClosed: false,
+        hasClosed: true
+      })
+    })
+  })
+
+  describe('with `closingDelay`, `isCollapsible` and `withAutomaticClosing`', () => {
+    it('should call both `onAutoClose()` and `onClose()` when waiting for auto-closing', () => {
+      mountAndWait(<BaseStory closingDelay={250} isCollapsible withAutomaticClosing />)
+
+      cy.get('.Component-Banner').should('be.empty')
+
+      outputShouldBe({
+        hasAutoClosed: true,
+        hasClosed: true
+      })
+    })
+
+    it('should only call `onClose()` when clicking on closing button', () => {
+      mountAndWait(<BaseStory closingDelay={99999} isCollapsible withAutomaticClosing />)
+
+      cy.clickButton('Masquer')
+
+      cy.get('.Component-Banner').should('be.empty')
+
+      outputShouldBe({
+        hasAutoClosed: false,
+        hasClosed: true
+      })
     })
   })
 })
