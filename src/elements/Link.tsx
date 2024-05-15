34d03d3d2aaa95c102683c79c637d141dd781d74
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-05-15 16:36:20 +0200

    fix(elements): fix visited colors & add importants in Link

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -4,36 +4,43 @@ import styled from 'styled-components'
 export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement>
 
 export const Link = styled.a<LinkProps>`
-  color: ${p => p.theme.color.slateGray};
-  text-decoration: underline;
+  color: ${p => p.theme.color.slateGray} !important;
+  text-decoration: underline !important;
 
   svg {
-    color: ${p => p.theme.color.slateGray};
+    color: ${p => p.theme.color.slateGray} !important;
+  }
+
+  &:visited {
+    color: ${p => p.theme.color.slateGray} !important;
+    text-decoration: underline !important;
+
+    svg {
+      color: ${p => p.theme.color.slateGray} !important;
+    }
   }
 
   &:hover,
-  &._hover {
-    color: ${p => p.theme.color.blueYonder};
+  &._hover,
+  &:visited:hover,
+  &.visited._hover {
+    color: ${p => p.theme.color.blueYonder} !important;
+    text-decoration: underline !important;
 
     svg {
-      color: ${p => p.theme.color.blueYonder};
+      color: ${p => p.theme.color.blueYonder} !important;
     }
   }
 
   &:active,
-  &._active {
-    color: ${p => p.theme.color.blueGray};
+  &._active,
+  &:visited:active,
+  &.visited._active {
+    color: ${p => p.theme.color.blueGray} !important;
+    text-decoration: underline !important;
 
     svg {
-      color: ${p => p.theme.color.blueGray};
-    }
-  }
-
-  &:visited {
-    color: ${p => p.theme.color.slateGray};
-
-    svg {
-      color: ${p => p.theme.color.slateGray};
+      color: ${p => p.theme.color.blueGray} !important;
     }
   }
 `
