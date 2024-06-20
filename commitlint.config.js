d0a5d20b4cda28cda2c9c28ec539762c1ecc472d
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-06-20 13:16:43 +0200

    ci(commitlint): enforce feature scopes

---
+++ commitlint.config.js
@@ -0,0 +1,44 @@
+const FEATURE_SCOPES = [
+  'components',
+  'cypress',
+  'elements',
+  'entities',
+  'fields',
+  'formiks',
+  'hooks',
+  'icons',
+  'libs',
+  'symbols',
+  'tables',
+  'types',
+  'utils'
+]
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  $schema: 'https://json.schemastore.org/commitlintrc',
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: 'https://github.com/angular/angular/blob/main/CONTRIBUTING.md#commit',
+  plugins: ['selective-scope'],
+  rules: {
+    'body-max-length': [2, 'always', 'Infinity'],
+    'body-max-line-length': [2, 'always', 'Infinity'],
+    'footer-max-length': [2, 'always', 'Infinity'],
+    'footer-max-line-length': [2, 'always', 'Infinity'],
+    'header-max-length': [2, 'always', 120],
+    'selective-scope': [
+      2,
+      'always',
+      {
+        feat: FEATURE_SCOPES,
+        fix: FEATURE_SCOPES,
+        style: FEATURE_SCOPES
+      }
+    ],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'lint', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
+  }
+}
