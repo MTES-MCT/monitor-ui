4253739005523a6f145308db45982dda22ad515d
Maxime Perrault <maxperrault94@gmail.com>
2024-07-04 09:59:10 +0200

    fix(fields): isHidden with display none makes label unaccessible

--- src/elements/Label.tsx
+++ src/elements/Label.tsx
@@ -15,7 +15,6 @@ export const Label = styled.label.attrs<LabelProps, LabelProps>(props => ({
   color: ${p =>
     // eslint-disable-next-line no-nested-ternary
     p.disabled ? p.theme.color.lightGray : p.hasError ? p.theme.color.maximumRed : p.theme.color.slateGray};
-  display: ${p => (p.isHidden ? 'none' : 'block')};
   font-size: 13px;
   line-height: 1.3846;
   margin-bottom: 4px;
@@ -26,4 +25,14 @@ export const Label = styled.label.attrs<LabelProps, LabelProps>(props => ({
         content:" *";
         color: ${p.theme.color.maximumRed};
       }`}
+  ${p =>
+    p.isHidden &&
+    `
+  position:absolute;
+  left:-10000px;
+  top:auto;
+  width:1px;
+  height:1px;
+  overflow:hidden;
+    `}
 `
