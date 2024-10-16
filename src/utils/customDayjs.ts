d5235124249cd2dcda39a0cbffe701105248eaf4
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-16 08:20:13 +0200

    feat(utils): add relativeTime extension to customDayjs

--- src/utils/customDayjs.ts
+++ src/utils/customDayjs.ts
@@ -4,6 +4,7 @@ import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import localeData from 'dayjs/plugin/localeData'
 import quarterOfYear from 'dayjs/plugin/quarterOfYear'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import timezone from 'dayjs/plugin/timezone'
 import updateLocale from 'dayjs/plugin/updateLocale'
 import utc from 'dayjs/plugin/utc'
@@ -14,6 +15,7 @@ dayjs.extend(isSameOrAfter)
 dayjs.extend(isSameOrBefore)
 dayjs.extend(localeData)
 dayjs.extend(quarterOfYear)
+dayjs.extend(relativeTime)
 dayjs.extend(timezone)
 dayjs.extend(updateLocale)
 dayjs.extend(utc)
