06ab17cc724021c40cb31b8dfdb212ea14d1e4c7
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-24 16:34:16 +0200

    feat(tables): allow any object as collection item in DataTable data

--- src/tables/DataTable/index.tsx
+++ src/tables/DataTable/index.tsx
@@ -12,20 +12,15 @@ import { Td } from './Td'
 import { Th } from './Th'
 import { SimpleTable } from '../SimpleTable'
 
-import type { CollectionItem } from '../../types/definitions'
+import type { AnyObject } from '../../types/definitions'
 
-export type DataTableProps<T extends CollectionItem> = {
+export type DataTableProps<T extends AnyObject> = {
   columns: Array<ColumnDef<T>>
   data: T[] | undefined
   initialSorting: SortingState
   tableOptions?: TableOptions<T> | undefined
 }
-export function DataTable<T extends CollectionItem>({
-  columns,
-  data,
-  initialSorting,
-  tableOptions
-}: DataTableProps<T>) {
+export function DataTable<T extends AnyObject>({ columns, data, initialSorting, tableOptions }: DataTableProps<T>) {
   const [sorting, setSorting] = useState<SortingState>(initialSorting)
 
   const table = useReactTable({
@@ -66,7 +61,7 @@ export function DataTable<T extends CollectionItem>({
 
               <tbody>
                 {rows.map(row => (
-                  <SimpleTable.BodyTr key={row.id} data-id={row.getValue<number>('id')}>
+                  <SimpleTable.BodyTr key={row.id} data-id={'id' in row.original ? row.original.id : row.id}>
                     {row.getVisibleCells().map(cell => (
                       <Td key={cell.id} cell={cell} />
                     ))}
