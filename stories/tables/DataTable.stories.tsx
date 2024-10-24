e8ff6550401d56e0ce34c81be4bd28182d350b13
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-24 16:14:52 +0200

    docs(tables): remove useless generated doc exports from DataTable story

--- stories/tables/DataTable.stories.tsx
+++ stories/tables/DataTable.stories.tsx
@@ -1,46 +1,16 @@
 import { generateStoryDecorator } from '../../.storybook/utils/generateStoryDecorator'
+import {
+  FAKE_BASIC_TABLE_COLUMNS,
+  FAKE_BASIC_TABLE_DATA,
+  type FakeBasicTableDataItem
+} from '../../__mocks__/fake_table_columns_and_data'
 import { DataTable, type DataTableProps } from '../../src'
 
 import type { Meta } from '@storybook/react'
-import type { ColumnDef } from '@tanstack/react-table'
 
-const COLUMNS: Array<ColumnDef<(typeof DATA)[0]>> = [
-  {
-    accessorFn: row => row.id,
-    enableSorting: false,
-    header: () => 'ID',
-    id: 'id',
-    size: 64
-  },
-  {
-    accessorFn: row => row.lastName,
-    header: () => 'Last Name',
-    id: 'lastName'
-  },
-  {
-    accessorFn: row => row.firstName,
-    header: () => 'First Name',
-    id: 'firstName',
-    size: 240
-  }
-]
-
-export const DATA = [
-  { firstName: 'Alice', id: 1, lastName: 'Smith' },
-  { firstName: 'Bob', id: 2, lastName: 'Johnson' },
-  { firstName: 'Charlie', id: 3, lastName: 'Williams' },
-  { firstName: 'David', id: 4, lastName: 'Brown' },
-  { firstName: 'Emily', id: 5, lastName: 'Jones' },
-  { firstName: 'Fiona', id: 6, lastName: 'Garcia' },
-  { firstName: 'George', id: 7, lastName: 'Miller' },
-  { firstName: 'Hannah', id: 8, lastName: 'Davis' },
-  { firstName: 'Ivan', id: 9, lastName: 'Rodriguez' },
-  { firstName: 'Jenny', id: 10, lastName: 'Martinez' }
-]
-
-export const args: DataTableProps<(typeof DATA)[0]> = {
-  columns: COLUMNS,
-  data: DATA,
+const args: DataTableProps<FakeBasicTableDataItem> = {
+  columns: FAKE_BASIC_TABLE_COLUMNS,
+  data: FAKE_BASIC_TABLE_DATA,
   initialSorting: [
     {
       // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -51,7 +21,7 @@ export const args: DataTableProps<(typeof DATA)[0]> = {
 }
 
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-const meta: Meta<DataTableProps<(typeof DATA)[0]>> = {
+const meta: Meta<DataTableProps<FakeBasicTableDataItem>> = {
   title: 'Tables/DataTable',
   component: DataTable,
 
@@ -65,7 +35,7 @@ const meta: Meta<DataTableProps<(typeof DATA)[0]>> = {
 
 export default meta
 
-export function _DataTable(props: DataTableProps<(typeof DATA)[0]>) {
+export function _DataTable(props: DataTableProps<FakeBasicTableDataItem>) {
   return (
     <>
       <DataTable {...props} />
