d75f6d4fc015d1aff437ce13058e53bee30796ab
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-11-21 18:47:17 +0100

    fix(hooks): add window definition guard to NewWindowContext

--- src/hooks/useNewWindow/context.ts
+++ src/hooks/useNewWindow/context.ts
@@ -4,6 +4,9 @@ import type { NewWindowContextValue } from './types'
 
 export const NewWindowContext = createContext<NewWindowContextValue>({
   newWindowContainerRef: {
-    current: window.document.createElement('div')
+    // It seems monitor-ui bundle can eager-evaluate `NewWindowContext`
+    // in contexts which don't have `window` global defined.
+    // This any-forced `undefined` should never impact the real webapp initialization.
+    current: typeof window !== 'undefined' ? window.document.createElement('div') : (undefined as any)
   }
 })
