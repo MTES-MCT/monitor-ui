cd720d728fd134b9168937c3d41ca9f64eb95fe6
Maxime Perrault <maxperrault94@gmail.com>
2025-02-28 17:12:14 +0100

    feat(components): create link component with prefix mask

---
+++ stories/formiks/FormikLinkInput.stories.tsx
@@ -0,0 +1,60 @@
+// TODO Migrate this story to the new Storybook structure. Example: stories/components/Banner.stories.tsx.
+/* eslint-disable react-hooks/rules-of-hooks */
+
+import { action } from '@storybook/addon-actions'
+import { Formik } from 'formik'
+import { FormikLinkInput, type FormikLinkInputProps } from 'formiks/FormikLinkInput'
+import { omit } from 'lodash'
+import { useMemo, useState } from 'react'
+
+import { Output } from '../../.storybook/components/Output'
+import { generateStoryDecorator } from '../../.storybook/utils/generateStoryDecorator'
+import { FormikEffect } from '../../src'
+import LinkInputStoryMeta from '../fields/LinkInput.stories'
+
+import type { Meta } from '@storybook/react'
+
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+const meta: Meta<FormikLinkInputProps> = {
+  title: 'Formiks/FormikLinkInput',
+  component: FormikLinkInput,
+
+  argTypes: omit(LinkInputStoryMeta.argTypes, ['error', 'onChange', 'value']),
+
+  args: omit({ name: 'link', ...LinkInputStoryMeta.args }, ['error', 'onChange', 'value']),
+
+  decorators: [
+    generateStoryDecorator({
+      box: { width: 640 },
+      withBackgroundButton: true
+    })
+  ]
+}
+/* eslint-enable sort-keys-fix/sort-keys-fix */
+
+export default meta
+
+export function _FormikLinkInput(props: FormikLinkInputProps) {
+  const [outputValue, setOutputValue] = useState<
+    | {
+        myLinkInput?: string
+      }
+    | '∅'
+  >('∅')
+
+  const key = useMemo(() => props.name, [props.name])
+
+  return (
+    <>
+      <Formik key={key} initialValues={{}} onSubmit={action('onSubmit')}>
+        <>
+          <FormikEffect onChange={setOutputValue} />
+
+          <FormikLinkInput {...props} />
+        </>
+      </Formik>
+
+      {outputValue !== '∅' && <Output value={outputValue} />}
+    </>
+  )
+}
