da4143ad6737af980d276ca94cba382e5bee98b6
Maxime Perrault <maxperrault94@gmail.com>
2025-05-06 16:12:12 +0200

    fix(components): move checkpicker css

--- src/fields/shared/StyledRsuitePickerBox.ts
+++ src/fields/shared/StyledRsuitePickerBox.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import {
   getFieldBackgroundColorFactory,
@@ -8,16 +8,7 @@ import {
 
 import type { CommonPickerFieldStyleProps } from './types'
 
-export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
-  font-size: 13px !important;
-  position: relative;
-  user-select: none;
-  width: 100%;
-
-  * {
-    ${p => p.$isReadOnly && `cursor: default !important;`}
-  }
-
+export const getCheckPickerInputCss = (p: CommonPickerFieldStyleProps) => css`
   > .rs-picker-toggle-wrapper,
   > .rs-auto-complete {
     background-color: transparent;
@@ -37,9 +28,8 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
       /* Remove focus ring (Select) */
       box-shadow: none;
       font-size: 13px;
-      height: 30px;
       line-height: 1.3846;
-      padding: 3.5px 40px 0 8px !important;
+      padding: 3.5px 40px 3.5px 8px !important;
 
       &:hover,
       &._hover {
@@ -91,7 +81,7 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
 
           /* Selected value(s) */
           > .rs-picker-toggle-value {
-            color: ${p => p.theme.color.gunMetal};
+            color: ${p.theme.color.gunMetal};
             font-weight: 500;
 
             > .rs-picker-value-list {
@@ -103,7 +93,7 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
             /* Counter badge, if any */
             > .rs-picker-value-count {
               align-items: center;
-              background-color: ${p => p.theme.color.charcoal};
+              background-color: ${p.theme.color.charcoal};
               border-radius: 50%;
               display: flex;
               font-family: 'Open Sans', sans-serif !important;
@@ -138,20 +128,20 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
       }
     }
   }
-
+`
+export const getCheckPickerMenuCss = (p: CommonPickerFieldStyleProps) => css`
   > .rs-picker-popup {
-    border: solid 1px ${p => p.theme.color.gainsboro};
+    border: solid 1px ${p.theme.color.gainsboro};
     border-radius: 0;
     padding: 0;
 
-    ${p =>
-      p.$popupWidth
-        ? `
+    ${p.$popupWidth
+      ? `
       max-width: ${p.$popupWidth}px;
       min-width: ${p.$popupWidth}px !important;
       width: ${p.$popupWidth}px;
       `
-        : `
+      : `
       width: 100%;
     `};
 
@@ -159,13 +149,13 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
       padding: 8px;
 
       > .rs-input-group {
-        background-color: ${p => p.theme.color.white};
-        border: solid 1px ${p => p.theme.color.lightGray};
+        background-color: ${p.theme.color.white};
+        border: solid 1px ${p.theme.color.lightGray};
         border-radius: 0;
 
         > [role='searchbox'] {
-          color: ${p => p.theme.color.gunMetal};
-          caret-color: ${p => p.theme.color.gunMetal};
+          color: ${p.theme.color.gunMetal};
+          caret-color: ${p.theme.color.gunMetal};
           font-size: 13px;
           line-height: 1;
           padding: 1px 8px 5px 8px;
@@ -180,7 +170,7 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
           padding: 4px 8px 4px 4px;
 
           > svg {
-            color: ${p => p.theme.color.lightGray};
+            color: ${p.theme.color.lightGray};
           }
         }
       }
@@ -195,7 +185,7 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
       [role='option'],
       [role='treeitem'] {
         > .rs-picker-select-menu-item {
-          color: ${p => p.theme.color.gunMetal};
+          color: ${p.theme.color.gunMetal};
           line-height: 1;
           overflow: hidden;
           padding: 8px 8px 11px 8px;
@@ -203,8 +193,8 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
           white-space: nowrap;
 
           &:hover {
-            background-color: ${p => p.theme.color.blueYonder25};
-            color: ${p => p.theme.color.blueYonder};
+            background-color: ${p.theme.color.blueYonder25};
+            color: ${p.theme.color.blueYonder};
           }
 
           &.rs-picker-select-menu-item-active {
@@ -212,13 +202,13 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
             font-weight: 500;
 
             &:hover {
-              background-color: ${p => p.theme.color.blueYonder25};
+              background-color: ${p.theme.color.blueYonder25};
             }
           }
 
           &.rs-picker-select-menu-item-disabled {
             background-color: transparent;
-            color: ${p => p.theme.color.lightGray};
+            color: ${p.theme.color.lightGray};
           }
         }
 
@@ -227,7 +217,7 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
             min-height: auto;
 
             > label {
-              color: ${p => p.theme.color.gunMetal};
+              color: ${p.theme.color.gunMetal};
               font-size: 13px !important;
               line-height: 1;
               overflow: hidden;
@@ -242,8 +232,8 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
 
                 > .rs-checkbox-inner {
                   &:before {
-                    background-color: ${p => p.theme.color.gainsboro};
-                    border: solid 2px ${p => p.theme.color.lightGray};
+                    background-color: ${p.theme.color.gainsboro};
+                    border: solid 2px ${p.theme.color.lightGray};
                     border-radius: 0;
                   }
                 }
@@ -252,17 +242,17 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
           }
 
           &:hover {
-            background-color: ${p => p.theme.color.blueYonder25};
+            background-color: ${p.theme.color.blueYonder25};
 
             > .rs-checkbox-checker {
               > label {
-                color: ${p => p.theme.color.blueYonder};
+                color: ${p.theme.color.blueYonder};
 
                 > .rs-checkbox-wrapper {
                   > .rs-checkbox-inner {
                     &:before {
-                      background-color: ${p => p.theme.color.blueYonder25};
-                      border: solid 2px ${p => p.theme.color.blueYonder};
+                      background-color: ${p.theme.color.blueYonder25};
+                      border: solid 2px ${p.theme.color.blueYonder};
                     }
                   }
                 }
@@ -271,7 +261,7 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
           }
 
           &.rs-check-item-focus {
-            background-color: ${p => p.theme.color.blueYonder25};
+            background-color: ${p.theme.color.blueYonder25};
 
             > .rs-checkbox-checker {
               > label {
@@ -284,19 +274,19 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
             background-color: transparent;
 
             &.rs-check-item-focus {
-              background-color: ${p => p.theme.color.blueYonder25};
+              background-color: ${p.theme.color.blueYonder25};
             }
 
             > .rs-checkbox-checker {
               > label {
-                color: ${p => p.theme.color.gunMetal};
+                color: ${p.theme.color.gunMetal};
                 font-weight: 500;
 
                 > .rs-checkbox-wrapper {
                   > .rs-checkbox-inner {
                     &:before {
-                      background-color: ${p => p.theme.color.charcoal};
-                      border: solid 2px ${p => p.theme.color.charcoal};
+                      background-color: ${p.theme.color.charcoal};
+                      border: solid 2px ${p.theme.color.charcoal};
                     }
                   }
                 }
@@ -305,198 +295,17 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
 
             &:hover,
             &._hover {
-              background-color: ${p => p.theme.color.blueYonder25};
+              background-color: ${p.theme.color.blueYonder25};
 
               > .rs-checkbox-checker {
                 > label {
-                  color: ${p => p.theme.color.blueYonder};
+                  color: ${p.theme.color.blueYonder};
 
                   > .rs-checkbox-wrapper {
                     > .rs-checkbox-inner {
                       &:before {
-                        background-color: ${p => p.theme.color.blueYonder};
-                        border: solid 2px ${p => p.theme.color.blueYonder};
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    /** CheckTreePicker **/
-    > .rs-check-tree {
-      padding: 0;
-      margin: 0;
-
-      .rs-check-tree-node-custom-icon {
-        margin-top: -2px;
-      }
-
-      [role='treeitem'] {
-        display: flex;
-        align-items: center;
-        .rs-check-item {
-          width: 100%;
-          color: ${p => p.theme.color.gunMetal};
-          line-height: 1;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          .rs-check-item-focus {
-            background-color: none;
-          }
-
-          &.rs-picker-select-menu-item-active {
-            background-color: transparent;
-            font-weight: 500;
-
-            &:hover {
-              background-color: ${p => p.theme.color.blueYonder25};
-            }
-          }
-
-          &.rs-picker-select-menu-item-disabled {
-            background-color: transparent;
-            color: ${p => p.theme.color.lightGray};
-          }
-        }
-
-        > .rs-check-item {
-          /** !important because it's override by determinate checkbox **/
-          &.rs-checkbox-indeterminate {
-            &:hover {
-              .rs-checkbox-inner {
-                &:before {
-                  background-color: ${p => p.theme.color.blueYonder} !important;
-                  border: solid 2px ${p => p.theme.color.blueYonder} !important;
-                }
-              }
-            }
-            .rs-checkbox-inner {
-              &:before {
-                background-color: ${p => p.theme.color.charcoal} !important;
-                border: solid 2px ${p => p.theme.color.charcoal} !important;
-              }
-            }
-          }
-          &.rs-checkbox-disabled {
-            &:hover {
-              background-color: transparent !important;
-            }
-            label,
-            span {
-              background-color: transparent !important;
-              color: ${p => p.theme.color.lightGray} !important;
-            }
-            &:hover {
-              span:before {
-                background-color: ${p => p.theme.color.gainsboro} !important;
-                border: solid 2px ${p => p.theme.color.lightGray} !important;
-              }
-            }
-          }
-          > .rs-checkbox-checker {
-            min-height: auto;
-
-            > label {
-              color: ${p => p.theme.color.gunMetal};
-              font-size: 13px !important;
-              line-height: 1;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-              padding: 1px 0;
-
-              > .rs-check-tree-node-label-content {
-                margin-left: 38px;
-                background-color: initial;
-                color: initial;
-              }
-
-              > .rs-checkbox-wrapper {
-                height: auto;
-                left: 8px;
-                top: 7px !important;
-                margin-left: 14px;
-
-                > .rs-checkbox-inner {
-                  &:before {
-                    background-color: ${p => p.theme.color.gainsboro};
-                    border: solid 2px ${p => p.theme.color.lightGray};
-                    border-radius: 0;
-                    margin-right: 8px;
-                  }
-                }
-              }
-            }
-          }
-
-          &:hover {
-            background-color: ${p => p.theme.color.blueYonder25} !important;
-
-            > .rs-checkbox-checker {
-              > label {
-                color: ${p => p.theme.color.blueYonder};
-
-                > .rs-checkbox-wrapper {
-                  > .rs-checkbox-inner {
-                    &:before {
-                      background-color: ${p => p.theme.color.blueYonder25};
-                      border: solid 2px ${p => p.theme.color.blueYonder};
-                    }
-                  }
-                }
-              }
-            }
-          }
-
-          &.rs-check-item-focus {
-            background-color: ${p => p.theme.color.blueYonder25} !important;
-
-            > .rs-checkbox-checker {
-              > label {
-                font-weight: 400;
-              }
-            }
-          }
-
-          &.rs-checkbox-checked {
-            &.rs-check-item-focus {
-              background-color: ${p => p.theme.color.blueYonder25} !important;
-            }
-
-            > .rs-checkbox-checker {
-              > label {
-                color: ${p => p.theme.color.gunMetal};
-                font-weight: 500;
-
-                > .rs-checkbox-wrapper {
-                  > .rs-checkbox-inner {
-                    &:before {
-                      background-color: ${p => p.theme.color.charcoal};
-                      border: solid 2px ${p => p.theme.color.charcoal};
-                    }
-                  }
-                }
-              }
-            }
-
-            &:hover,
-            &._hover {
-              background-color: ${p => p.theme.color.blueYonder25};
-
-              > .rs-checkbox-checker {
-                > label {
-                  color: ${p => p.theme.color.blueYonder};
-
-                  > .rs-checkbox-wrapper {
-                    > .rs-checkbox-inner {
-                      &:before {
-                        background-color: ${p => p.theme.color.blueYonder};
-                        border: solid 2px ${p => p.theme.color.blueYonder};
+                        background-color: ${p.theme.color.blueYonder};
+                        border: solid 2px ${p.theme.color.blueYonder};
                       }
                     }
                   }
@@ -508,4 +317,18 @@ export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
       }
     }
   }
+`
+export const StyledRsuitePickerBox = styled.div<CommonPickerFieldStyleProps>`
+  font-size: 13px !important;
+  position: relative;
+  user-select: none;
+  width: 100%;
+
+  * {
+    ${p => p.$isReadOnly && `cursor: default !important;`}
+  }
+
+  ${p => getCheckPickerInputCss(p)}
+
+  ${p => getCheckPickerMenuCss(p)}
 `
