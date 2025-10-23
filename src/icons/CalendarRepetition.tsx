309c37edf832f8dcb2e5744a50da7d4692a00be8
Maxime Perrault <maximeperrault.dev@gmail.com>
2025-10-23 17:17:01 +0200

    fix(icons): modifying CalendarRepetition.svg

--- src/icons/CalendarRepetition.tsx
+++ src/icons/CalendarRepetition.tsx
@@ -8,17 +8,24 @@ export function CalendarRepetition({ color, size, title, ...nativeProps }: IconP
       <svg height="20" viewBox="0 0 20 20" width="20" {...nativeProps}>
         <defs>
           <clipPath>
+            <rect fill="currentColor" height="20" transform="translate(0 0)" width="20" />
+          </clipPath>
+          <clipPath>
             <rect fill="none" height="20" width="20" />
           </clipPath>
         </defs>
-        <path d="M12,0V2H5V0H3V2H0V18H9V16H2V8H15V9h2V2H14V0Z" fill="currentColor" transform="translate(0)" />
-        <g transform="translate(0)">
+        <g transform="translate(0 0)">
           <g clipPath="url(#clip-path)">
             <path
-              d="M18.5,10.5v.971A4.918,4.918,0,0,0,15,10a5,5,0,1,0,4.745,6.5H17.583a3,3,0,1,1-.366-3.5H16v1.5h4v-4Z"
+              d="M18.5,10.5v.971A4.917,4.917,0,0,0,15,10a5,5,0,1,0,4.744,6.5H17.582a3,3,0,1,1-.366-3.5H16v1.5h4v-4H18.5Z"
               fill="currentColor"
             />
-            <rect fill="#fff" height="2" transform="translate(2 4)" width="13" />
+            <path d="M14,2V0H12V2H5V0H3V2H0V18H9V16H2V8H15V9h2V2Zm1,4H2V4H15Z" fill="currentColor" />
+          </g>
+        </g>
+        <g transform="translate(0 0)">
+          <g clipPath="url(#clip-path-2)">
+            <rect fill="none" height="2" transform="translate(2 4)" width="13" />
           </g>
         </g>
       </svg>
