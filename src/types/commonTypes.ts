8bea7b37cf295a583bb13bb55b3a5a44a512b201
Maxime Perrault <maximeperrault.dev@gmail.com>
2026-03-10 17:27:57 +0100

    feat(fields): refactor MultiSelect.tsx - renderMenu is customizable - expose callback function onSearch - reload component when options change asynchonously

--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -1,8 +1,11 @@
+import React from 'react'
+
 import type { Option, OptionValueType } from './definitions'
 import type { CustomSearch } from '@libs/CustomSearch'
 import type { Promisable } from 'type-fest'
 
 export interface SelectType<OptionValue extends OptionValueType = string> {
+  customRenderMenuItem?: (label: React.ReactNode, item: any) => React.ReactNode
   customSearch?: CustomSearch<Option<OptionValue>>
   /** Minimum search query length required to trigger custom search filtering. */
   customSearchMinQueryLength?: number | undefined
@@ -15,7 +18,8 @@ export interface SelectType<OptionValue extends OptionValueType = string> {
   isUndefinedWhenDisabled?: boolean | undefined
   label: string
   name: string
-  onChange?: (nextValue: OptionValue[] | undefined) => Promisable<void>
+  onChange?: (nextValue: OptionValue[] | undefined) => void
+  onSearch?: (search: string) => Promisable<void>
   optionValueKey?: keyof OptionValue | undefined
   options: Option<OptionValue>[]
   popupWidth?: number | undefined
